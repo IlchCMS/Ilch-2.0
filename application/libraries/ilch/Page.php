1ea81f6752e2680fd553e29bf119d4802f27d24b
Dominik Meyer <kinimodmeyer@gmail.com>
2013-08-19 23:44:25 +0200

    create loader and page class

---
+++ application/libraries/ilch/Page.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Dominik Meyer <kinimodmeyer@gmail.com>
+ * @copyright Ilch 2.0
+ * @package ilch
+ */
+class Ilch_Page
+{
+}
--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 /**
- * Ilch 2.0
+ * Load and initialize all needed classes.
+ *
+ * @author Dominik Meyer <kinimodmeyer@gmail.com>
+ * @copyright Ilch 2.0
+ * @package ilch
  */
+
+define('ACCESS', 1);
+define('APPLICATION_PATH', '/');
+
+require_once APPLICATION_PATH.'/libraries/ilch/Loader.php';
+
+$page = new Ilch_Page();
+$page->generatePage();
