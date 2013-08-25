d409edc976b2a510c2482967dc3457425d47e425
Dominik Meyer <kinimodmeyer@gmail.com>
2013-08-25 22:34:31 +0200

    remove config adding to gitignore

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config['dbHost'] = 'localhost';
-$config['dbUser'] = 'root';
-$config['dbPassword'] = '';
-$config['dbName'] = 'ilch';
-$config['dbPrefix'] = 'ilch';
-$config['dbEngine'] = 'Mysql'
-?>
