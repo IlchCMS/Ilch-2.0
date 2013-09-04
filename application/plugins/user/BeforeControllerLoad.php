f078ea31a0413681092e0c1291e6316b224a5e30
mjainta <martin.jainta@gmail.com>
2013-09-04 21:22:53 +0200

    BeforeControllerLoad user plugin to set user model in registry using $_SESSION

--- application/plugins/user/BeforeControllerLoad.php
+++ application/plugins/user/BeforeControllerLoad.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Holds class User_BeforeControllerLoadPlugin.
+ *
  * @author Meyer Dominik
  * @copyright Ilch CMS 2.0
  * @package ilch
@@ -7,10 +9,39 @@
 
 defined('ACCESS') or die('no direct access');
 
+/**
+ * Does user operations before the controller loads.
+ *
+ * @author  Martin Jainta
+ * @copyright Ilch CMS 2.0
+ * @package ilch
+ */
 class User_BeforeControllerLoadPlugin
 {
+	/**
+	 * Checks if a user id was given in the request and sets the user.
+	 *
+	 * If no user id is given a default user will be created.
+	 */
 	public function __construct()
 	{
-		//TBD
+		$userId = null;
+
+		if(isset($_SESSION['user_id']))
+		{
+			$userId = (int)$_SESSION['user_id'];
+		}
+
+		$mapper = new User_UserMapper();
+		$user = $mapper->getUserById($userId);
+
+		if($user === null)
+		{
+			$user = new User_UserModel();
+			$user->setId(0);
+			$user->setName('Guest');
+		}
+
+		Ilch_Registry::set('user', $user);
 	}
 }
