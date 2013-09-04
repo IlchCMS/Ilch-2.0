321489c1950a3cdefe96a4ffb63a8036e766c541
mjainta <martin.jainta@gmail.com>
2013-09-04 23:46:21 +0200

    Adding user table to schema.

--- application/modules/install/files/install_general.sql
+++ application/modules/install/files/install_general.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_confirmed` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
