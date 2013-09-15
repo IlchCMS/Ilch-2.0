1d58cbd12880c25050ca1e539263f0612b124f0e
Dominik Meyer <kinimodmeyer@gmail.com>
2013-09-15 16:23:26 +0200

    follow up adding maintenance

--- application/modules/admin/controllers/admin/Settings.php
+++ application/modules/admin/controllers/admin/Settings.php
@@ -11,12 +11,14 @@ class Admin_Admin_SettingsController extends Ilch_Controller_Admin
 {
 	public function indexAction()
 	{
-		$this->getView()->set('languages', $this->getTranslator()->getLocaleList());
-		
 		if($this->getRequest()->isPost())
 		{
 			$this->getConfig()->set('locale', $this->getRequest()->getPost('language'));
-			$this->redirect(array('action' => 'index'));
+			$this->getTranslator()->setLocale($this->getRequest()->getPost('language'));
+			$this->getConfig()->set('maintenance_mode', $this->getRequest()->getPost('maintenanceMode'));
 		}
+
+		$this->getView()->set('languages', $this->getTranslator()->getLocaleList());
+		$this->getView()->set('maintenanceMode', $this->getConfig()->get('maintenance_mode'));
 	}
 }
