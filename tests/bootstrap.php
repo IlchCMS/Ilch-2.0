95d72e06a32ee50ec9effdb240b68fd9eebf780e
mjainta <martin.jainta@gmail.com>
2013-10-22 23:02:58 +0200

    Tests: Improved phpunit.xml/config.php handling further.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,7 +12,7 @@
 define('ACCESS', 1);
 define('VERSION', '2.0');
 define('APPLICATION_PATH', __DIR__.'/../application');
-define('CONFIG_PATH', __DIR__);
+define('CONFIG_PATH', __DIR__.'/..');
 define('SERVER_TIMEZONE', 'Europe/Berlin');
 
 /*
