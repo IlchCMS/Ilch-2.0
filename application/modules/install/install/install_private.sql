495f5226bee210c6a3543a6e76b64ba844a393de
kinimodmeyer <kinimodmeyer@gmail.com>
2013-12-01 16:51:19 +0100

    adding english private startpage content.

--- application/modules/install/install/install_private.sql
+++ application/modules/install/install/install_private.sql
@@ -1,5 +1,8 @@
 INSERT INTO `ilch_pages_content` (`page_id`, `content`, `locale`, `title`, `perma`) VALUES
-(1, '<p><strong>Guten Tag und willkommen auf meiner Internetseite!</strong></p>\r\n<p>&nbsp;</p>\r\n<p>Auf dieser Seite m&ouml;chte ich mich als Person vorstellen sowie einen einen Blog f&uuml;hren.</p>', 'de_DE', 'Startseite', 'startseite.html');
+(1, '<p><strong>Guten Tag und willkommen auf meiner Internetseite!</strong></p>\r\n\r\n<p>Auf dieser Seite möchte ich mich als Person vorstellen.</p>', 'de_DE', 'Startseite', 'startseite.html');
+
+INSERT INTO `ilch_pages_content` (`page_id`, `content`, `locale`, `title`, `perma`) VALUES
+(1, '<p><strong>Welcome on my Page!</strong></p>\r\n\r\n<p>On this site i want to present me as a person.</p>', 'en_EN', 'Startpage', 'startpage.html');
 
 INSERT INTO `ilch_pages` (`id`, `date_created`) VALUES
 (1, 'CURRENT_TIMESTAMP');
