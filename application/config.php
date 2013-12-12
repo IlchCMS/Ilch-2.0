3222b29dba458edd132ef4f87d1b2e01bca1968c
mjainta <martin.jainta@gmail.com>
2013-12-12 17:26:32 +0100

    travis.yml: Setting up database for travis ci.

---
+++ application/config.php
@@ -0,0 +1,8 @@
+<?php
+$config["dbEngineTest"] = "Mysql";
+$config["dbHostTest"] = "127.0.0.1";
+$config["dbPrefixTest"] = "";
+$config["dbUserTest"] = "travis"; // Config for tests
+$config["dbPasswordTest"] = ""; // Config for tests
+$config["dbNameTest"] = "ilch2_test"; // Config for tests
+?>
