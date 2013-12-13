05509cc3a98e43ff394ca6c0ffbbf92449557786
c0r1an <th.stantin@gmx.de>
2013-12-13 19:52:45 +0100

    adds strtolower to rewritebaseparts

---
+++ application/modules/guestbook/translations/de.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Translations file for the admin module.
+ *
+ * @copyright Ilch 2.0
+ * @package ilch
+ */
+
+defined('ACCESS') or die('no direct access');
+
+return array
+(
+    'menuActionNewUser' => 'Neuen Benutzer hinzufügen',
+    'menuActionNewGroup' => 'Neue Gruppe hinzufügen',
+
+    'menuUser' => 'Benutzer',
+    'menuGroup' => 'Gruppen',
+
+    'treat' => 'Bearbeiten',
+    'addUser' => 'Benutzer hinzufügen',
+    'editUser' => 'Benutzer editieren',
+    'deleteUser' => 'Benutzer entfernen',
+    'userName' => 'Benutzername',
+    'userEmail' => 'E-Mail Adresse',
+    'userDateCreated' => 'Erstellt',
+    'userDateConfirmed' => 'Bestätigt',
+    'userDateLastActivity' => 'Zuletzt Online',
+    'noUsersExist' => 'Keine Benutzer vorhanden.',
+    'filter' => 'Tabellenfilter',
+    'search' => 'Suchen',
+    'userGroups' => 'Benutzergruppen',
+    'save' => 'Speichern',
+    'notConfirmedYet' => 'Noch nicht bestätigt',
+    'neverLoggedIn' => 'Noch nicht eingeloggt',
+    'noGroupsAssigned' => 'Keine Gruppe zugewiesen',
+    'assignedGroups' => 'Zugewiesene Gruppen',
+    'selectAssignedGroups' => 'Zugewiesene Gruppen wählen',
+    'askIfDeleteUser' => 'Sind Sie sicher dass der Benutzer "%s" entfernt werden soll?',
+    'newUserMsg' => 'Der Benutzer wurde erfolgreich angelegt.',
+    'delUserMsg' => 'Der Benutzer wurde erfolgreich entfernt.',
+    'delLastAdminProhibited' => 'Es ist nicht erlaubt den letzten aktiven Benutzer mit zugewiesener Gruppe "Administrator" zu entfernen.',
+    'delOwnUserProhibited' => 'Es ist nicht erlaubt Ihren eigenen Benutzer zu entfernen.',
+
+    'groupName' => 'Gruppenname',
+    'groupAssignedUsers' => 'Anzahl "Zugeordnete Benutzer"',
+    'editGroup' => 'Gruppe editieren',
+    'addGroup' => 'Gruppe hinzufügen',
+    'newGroupMsg' => 'Die Gruppe wurde erfolgreich angelegt.',
+    'askIfDeleteGroup' => 'Sind Sie sicher dass die Gruppe "%s" entfernt werden soll?',
+    'delGroupMsg' => 'Die Gruppe wurde erfolgreich entfernt.',
+    'delAdminGroup' => 'Es ist nicht erlaubt die Gruppe "Administrator" zu entfernen.',
+    'noGroupsExist' => 'Keine Gruppen vorhanden.',
+);
---
+++ application/modules/guestbook/translations/en.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Translations file for the admin module.
+ *
+ * @copyright Ilch 2.0
+ * @package ilch
+ */
+
+defined('ACCESS') or die('no direct access');
+
+return array
+(
+    'menuActionNewUser' => 'Create new user',
+    'menuActionNewGroup' => 'Create new group',
+
+    'menuUser' => 'User',
+    'menuGroup' => 'Groups',
+
+    'treat' => 'Edit',
+    'addUser' => 'Create user',
+    'editUser' => 'Edit user',
+    'deleteUser' => 'Remove user',
+    'userName' => 'Username',
+    'userEmail' => 'Email address',
+    'userDateCreated' => 'Created',
+    'userDateConfirmed' => 'Confirmed',
+    'userDateLastActivity' => 'Last Online',
+    'noUsersExist' => 'No users exist.',
+    'filter' => 'Tablefilters',
+    'search' => 'Search',
+    'userGroups' => 'Usergroups',
+    'save' => 'Save',
+    'notConfirmedYet' => 'Not confirmed, yet',
+    'neverLoggedIn' => 'Not logged in, yet',
+    'noGroupsAssigned' => 'No group assigned',
+    'assignedGroups' => 'Assigned groups',
+    'selectAssignedGroups' => 'Select assigned groups',
+    'askIfDeleteUser' => 'Are you sure to delete user "%s"?',
+    'newUserMsg' => 'User got created successfully.',
+    'delUserMsg' => 'User got deleted successfully.',
+    'delLastAdminProhibited' => 'It is not allowed to delete the last user with the group "Administrator" assigned.',
+    'delOwnUserProhibited' => 'It is not allowed to delete the your own user.',
+
+    'groupName' => 'Groupname',
+    'groupAssignedUsers' => 'Number "Assigned users"',
+    'editGroup' => 'Edit group',
+    'addGroup' => 'Create user',
+    'newGroupMsg' => 'Group got created successfully.',
+    'askIfDeleteGroup' => 'Are you sure to delete group "%s"?',
+    'delGroupMsg' => 'Group got deleted successfully.',
+    'delAdminGroup' => 'It is not allowed to delete the group "Administrator".',
+    'noGroupsExist' => 'No groups exist.',
+);
