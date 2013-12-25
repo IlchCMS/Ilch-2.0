4f08cbf220b2514c6c09103ed54e0770fe4dc8c7
mjainta <martin.jainta@gmail.com>
2013-12-26 00:44:41 +0100

    PHPUnit: DB-Config for tests now required for DB-Tests. (Redmine 144)

--- tests/PHPUnit/Ilch/DatabaseTestCase.php
+++ tests/PHPUnit/Ilch/DatabaseTestCase.php
@@ -68,6 +68,13 @@ class PHPUnit_Ilch_DatabaseTestCase extends PHPUnit_Extensions_Database_TestCase
 
         $this->db = $dbFactory->getInstanceByConfig(Registry::get('config'));
 
+        if($this->db === false)
+        {
+            $this->markTestIncomplete('Necessary DB configuration is not set.');
+            parent::setUp();
+            return;
+        }
+
         /*
          * Deleting all tables from the db and setting up the db using the given schema.
          */
@@ -88,35 +95,36 @@ class PHPUnit_Ilch_DatabaseTestCase extends PHPUnit_Extensions_Database_TestCase
     /**
      * Creates the db connection to the test database.
      *
-     * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
+     * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection|null Returns null if the necessary config was not set.
      */
     final public function getConnection()
     {
         $dbData = array();
         $config = Registry::get('config');
 
-        foreach (array('dbEngine', 'dbHost', 'dbUser', 'dbPassword', 'dbName', 'dbPrefix') as $configKey) {
+        foreach (array('dbEngineTest', 'dbHostTest', 'dbUserTest', 'dbPasswordTest', 'dbNameTest', 'dbPrefixTest') as $configKey) {
             /*
              * Using the data for the db from the config.
              * We check if special config variables for this test execution exist.
-             * If so we gonna use it.
+             * If so we gonna use it. Otherwise we have to skip the tests.
              */
-            if ($config->get($configKey.'Test') !== null) {
-                $dbData[$configKey] = $config->get($configKey.'Test');
-            } elseif ($config->get($configKey) !== null) {
+            if ($config->get($configKey) !== null) {
                 $dbData[$configKey] = $config->get($configKey);
+            } else {
+                $this->markTestIncomplete('Necessary DB configuration is not set.');
+                return;
             }
         }
 
-        $dsn = strtolower($dbData['dbEngine']).':dbname='.$dbData['dbName'].';host='.$dbData['dbHost'];
-        $dbData['dbDsn'] = $dsn;
+        $dsn = strtolower($dbData['dbEngineTest']).':dbname='.$dbData['dbNameTest'].';host='.$dbData['dbHostTest'];
+        $dbData['dbDsnTest'] = $dsn;
 
         if ($this->conn === null) {
             if (self::$pdo === null) {
-                self::$pdo = new PDO($dbData['dbDsn'], $dbData['dbUser'], $dbData['dbPassword']);
+                self::$pdo = new PDO($dbData['dbDsnTest'], $dbData['dbUserTest'], $dbData['dbPasswordTest']);
             }
 
-            $this->conn = $this->createDefaultDBConnection(self::$pdo, $dbData['dbName']);
+            $this->conn = $this->createDefaultDBConnection(self::$pdo, $dbData['dbNameTest']);
         }
 
         return $this->conn;
