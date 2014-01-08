57d6354a8da33933e146f34f732d05abf55f74be
c0r1an <th.stantin@gmx.de>
2014-01-09 00:20:08 +0100

    add media modul

---
+++ application/modules/media/translations/en.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Translations file for the admin module.
+ *
+ * @copyright Ilch 2.0
+ * @package ilch
+ */
+
+defined('ACCESS') or die('no direct access');
+
+return array
+(
+    'media' => 'Media',
+    'menuMedia' => 'Media',
+    'allMedias' => 'all Media',
+    'menuActionAddNew' => 'Add New',
+    'description' => 'Description',
+    'mediatype' => 'Media type',
+    'options' => 'Options',
+    'mediaUpload' => 'Media Upload',
+);
