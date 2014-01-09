929b0f92349eb0a8eb0ef57e43a8949aab858baf
kveldscholten <kevin_veldscholten@online.de>
2014-01-09 18:26:11 +0100

    truncate config

--- application/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config["dbEngine"] = "Mysql";
-$config["dbHost"] = "localhost";
-$config["dbUser"] = "root";
-$config["dbPassword"] = "";
-$config["dbName"] = "ilch2";
-$config["dbPrefix"] = "ilch_";
-?>
