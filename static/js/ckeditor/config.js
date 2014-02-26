980075609640f7e6ff50c690c4963da474fe9253
kinimodmeyer <kinimodmeyer@gmail.com>
2014-02-26 12:27:58 +0100

    fix editor for frontend

--- static/js/ckeditor/config.js
+++ static/js/ckeditor/config.js
@@ -4,31 +4,8 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For the complete reference:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
-
-	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' },
-		{ name: 'ilchmedia' }
-	];
-
-	// Se the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
-	CKEDITOR.plugins.addExternal( 'ilchmedia', ilchMediaPlugin);
-	config.extraPlugins = 'ilchmedia';
+    if (typeof ilchMediaPlugin != "undefined") {
+        CKEDITOR.plugins.addExternal( 'ilchmedia', ilchMediaPlugin);
+        config.extraPlugins = 'ilchmedia';
+    }
 };
