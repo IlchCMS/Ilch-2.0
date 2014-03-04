f37ddb1791629703bcbc03396423f44c1e2be9be
kinimodmeyer <kinimodmeyer@gmail.com>
2014-03-05 00:27:09 +0100

    change gaming to clan2columns

---
+++ application/layouts/clan2columns/config/config.php
@@ -0,0 +1,4 @@
+<?php
+$config['key'] = 'Ilch-Clan';
+$config['author'] = 'Ilch.de';
+$config['desc'] = '2 Spalten Clan Layout';
--- application/layouts/gaming/config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-$config['key'] = 'Ilch-Gaming';
-$config['author'] = 'Ilch.de';
-$config['desc'] = '2 Spalten Gaming Layout';
