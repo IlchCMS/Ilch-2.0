e8fdfee17d07e96d03265c818ceb07dd7d3e50fd
kinimodmeyer <kinimodmeyer@gmail.com>
2014-03-08 15:24:29 +0100

    remove brand on gaming layout

--- application/layouts/clan2columns/index.php
+++ application/layouts/clan2columns/index.php
@@ -29,7 +29,6 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="/">Brand</a>
                     </div>
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav navbar-right">
