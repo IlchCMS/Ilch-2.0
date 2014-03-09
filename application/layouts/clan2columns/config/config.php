c8b86a330c4b8f3180f812640592d0824d47536d
kinimodmeyer <kinimodmeyer@gmail.com>
2014-03-09 12:44:44 +0100

    adding layout overrides

--- application/layouts/clan2columns/config/config.php
+++ application/layouts/clan2columns/config/config.php
@@ -2,3 +2,4 @@
 $config['key'] = 'Ilch-Clan';
 $config['author'] = 'Ilch.de';
 $config['desc'] = '2 Spalten Clan Layout';
+$config['layouts']['index_full'] = array(array('module' => 'guestbook'));
--- application/libraries/Ilch/Controller/Frontend.php
+++ application/libraries/Ilch/Controller/Frontend.php
@@ -12,12 +12,28 @@ class Frontend extends Base
     public function __construct(\Ilch\Layout\Base $layout, \Ilch\View $view, \Ilch\Request $request, \Ilch\Router $router, \Ilch\Translator $translator)
     {
         parent::__construct($layout, $view, $request, $router, $translator);
-
+        
         if (!empty($_SESSION['layout'])) {
-            $this->getLayout()->setFile('layouts/'.$_SESSION['layout'].'/index', $_SESSION['layout']);
+            $layoutKey = $_SESSION['layout'];
+            $layoutFile = 'index';
         } elseif($this->getConfig() !== NULL) {
-            $defaultLayout = $this->getConfig()->get('default_layout');
-            $this->getLayout()->setFile('layouts/'.$defaultLayout.'/index', $defaultLayout);
+            $layoutKey = $this->getConfig()->get('default_layout');
+            $layoutFile = 'index';
         }
+
+        require_once APPLICATION_PATH.'/layouts/'.$layoutKey.'/config/config.php';
+        
+        if(!empty($config['layouts'])) {
+            foreach ($config['layouts'] as $layoutKeyConfig => $layouts) {
+                foreach ($layouts as $url) {
+                    if($url['module'] == $this->getRequest()->getModuleName()) {
+                        $layoutFile = $layoutKeyConfig;
+                        break 2;
+                    }
+                }
+            }
+        }
+
+        $this->getLayout()->setFile('layouts/'.$layoutKey.'/'.$layoutFile, $layoutKey);
     }
 }
