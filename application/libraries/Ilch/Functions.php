e82ecc88f05284d11fb74f4061a8b96075599cf2
Tobias Schwarz <tobias.schwarz@gmx.eu>
2014-05-12 18:20:11 +0200

    Adds a Validator class

--- application/libraries/Ilch/Functions.php
+++ application/libraries/Ilch/Functions.php
@@ -81,3 +81,36 @@ function removeDir($dir)
         unlink($dir);
     }
 }
+
+/**
+ * Gets array data
+ *
+ * Supports 'dot' notation for arrays
+ * e.g.
+ *      foo.bar     > foo['bar']
+ *      foo.bar.baz > foo['bar']['baz']
+ *
+ * @param array     $data       The array
+ * @param string    $key        The key to look for
+ * @param mixed     $default    A default value if $key is not found
+ */
+function array_dot($data = array(), $key = null, $default = null)
+{
+    if ($key === null) {
+        return $data;
+    }
+
+    if (isset($data[$key])) {
+        return $data[$key];
+    }
+
+    foreach (explode('.', $key) as $seg) {
+        if (!is_array($data) || !array_key_exists($seg, $data)) {
+            return $default;
+        }
+
+        $data = $data[$seg];
+    }
+
+    return $data;
+}
---
+++ application/libraries/Ilch/Validators/Required.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Required validation class
+ *
+ * @copyright Ilch 2.0
+ * @package ilch
+ * @author Tobias Schwarz <tobias.schwarz@gmx.eu>
+ */
+
+namespace Ilch\Validators;
+
+class Required extends Base
+{
+    /**
+     * @var mixed The value to validate
+     */
+    protected $value;
+
+    /**
+     * Prepares the validator
+     *
+     * @param mixed   $value       The value to validate
+     * @param array   $source      The whole source array
+     * @param integer $params      Parameters
+     */
+    public function prepare($value, $source, $params)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * Executes the validation
+     */
+    public function execute()
+    {
+        if (empty($this->value)) {
+            $this->addError('ist ein Pflichtfeld.');
+        }
+    }
+}
