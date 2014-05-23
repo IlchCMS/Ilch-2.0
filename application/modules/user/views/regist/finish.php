b35d67b9e3c0c6f8c5fd58f0b74af763d785eb72
Thomas Stantin <th.stantin@gmx.de>
2014-05-23 19:28:53 +0200

    Update finish.php

--- application/modules/user/views/regist/finish.php
+++ application/modules/user/views/regist/finish.php
@@ -4,7 +4,7 @@
             <i class="fa fa-check-circle text-success" title=""></i>
         </div>
         <div class="col-lg-11">
-            Vielen Dank <b><?php echo $_SESSION["name"]; ?></b>, dass Sie sich registiert haben.<br />
+            Vielen Dank <b><?php echo $_SESSION["name"]; ?></b>, dass Sie sich registriert haben.<br />
             Es wurde eine E-Mail an <b><?php echo $_SESSION["email"]; ?></b> geschickt, mit Anweisungen, wie Sie Ihr Benutzerkonto aktivieren können.
         </div>
     </div>
@@ -14,7 +14,7 @@
             <i class="fa fa-check-circle text-success" title=""></i>
         </div>
         <div class="col-lg-11">
-            Vielen Dank <b><?php echo $_SESSION["name"]; ?></b>, dass Sie sich registiert haben.<br />
+            Vielen Dank <b><?php echo $_SESSION["name"]; ?></b>, dass Sie sich registriert haben.<br />
             Sie können sich jetzt mit Ihren Benutzerdaten anmelden.
         </div>
     </div>
