4d0e993626564e5b12a9770ece16c8926700264f
Thomas Stantin <th.stantin@gmx.de>
2014-05-25 10:09:11 +0200

    Update Thumbnail.php

--- application/libraries/Thumb/Thumbnail.php
+++ application/libraries/Thumb/Thumbnail.php
@@ -1,5 +1,27 @@
 <?php
 
+/**
+
+EasyPhpThumbnail class version 2.0.6 - PHP5
+On-the-fly image manipulation and thumbnail generation
+
+Copyright (c) 2008-2012 JF Nutbroek <jfnutbroek@gmail.com>
+Visit http://www.mywebmymail.com for more information and a commercial version
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+*/
+
 namespace Thumb;
 
 defined('ACCESS') or die('no direct access');
