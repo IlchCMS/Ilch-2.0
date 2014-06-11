5caf87594011c4e54ca7dee816b870819550ed83
Dominik Meyer <kinimodmeyer@gmail.com>
2014-06-11 18:33:33 +0200

    adding model __call

--- application/libraries/Ilch/Model.php
+++ application/libraries/Ilch/Model.php
@@ -24,4 +24,32 @@ class Model
             }
         }
     }
+
+    /**
+     * Dynamic gets/set properties for not defined getter/setter.
+     *
+     * @param string $name
+     * @param mixed[] $arguments
+     * @return mixed[]
+     */
+    public function __call($name, $arguments) 
+    {
+        $action = substr($name, 0, 3);
+        $property = lcfirst(substr($name, 3));
+
+        if ($action == 'get' || $action == 'set') {
+            if (property_exists($this, $property)) {
+                if ($action == 'get') {
+                    return $this->$property;
+                } else {
+                    /*
+                     * @todo implement more arguments.
+                     */
+                    $this->$property = $arguments[0];
+                }
+            } else {
+                throw new \BadMethodCallException('property "' . $property . '" not defined');
+            }
+        }
+    }
 }
