58037af020a1a7aad71a013d1e262d550f584eae
Tobias Schwarz <tobias.schwarz@gmx.eu>
2014-11-25 20:56:40 +0100

    Matches: Adds a dummy IndexController

---
+++ application/modules/matches/controllers/admin/Index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Matches index controller
+ *
+ * @copyright Ilch 2.0
+ * @package ilch
+ */
+
+namespace Modules\Matches\Controllers\Admin;
+
+use \Modules\Matches\Exceptions\DatabaseException;
+
+class Index extends Base
+{
+    public function indexAction()
+    {
+
+    }
+}
