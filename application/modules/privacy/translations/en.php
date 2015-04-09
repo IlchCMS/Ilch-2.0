9f4b5ecef361b9b369f82bec4407d587408db734
kveldscholten <kevin_veldscholten@online.de>
2015-04-09 03:29:58 +0200

    add PrivacyModules, change ContactModules to systemstandard Modules

---
+++ application/modules/privacy/translations/en.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @copyright Ilch 2.0
+ * @package ilch
+ */
+
+defined('ACCESS') or die('no direct access');
+
+return array
+(
+    'privacy' => 'Privacy Policy',
+    'title' => 'Title',
+    'urlTitle' => 'Titel of Source',
+    'url' => 'Link',
+    'privacyTitle' => 'Title Source',
+    'text' => 'Text',
+    'menuPrivacy' => 'Privacy Policy',
+    'menuActionNewPrivacy' => 'New Privacy Policy',
+    'menuActionEditPrivacy' => 'Edit Privacy Policy',
+    'managePrivacy' => 'Manage Privacy Policy',
+    'noPrivacy' => 'No Privacy Policy available',
+    'manage' => 'Manage',
+);
