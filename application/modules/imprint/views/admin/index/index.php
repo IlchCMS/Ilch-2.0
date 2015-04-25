af65e5db735e46f4cdc51a1812cb31669c51631f
kveldscholten <kevin_veldscholten@online.de>
2015-04-25 21:32:42 +0200

    Update ImprintModules

--- application/modules/imprint/views/admin/index/index.php
+++ application/modules/imprint/views/admin/index/index.php
@@ -1,91 +1,110 @@
 <form class="form-horizontal" method="POST" action="<?=$this->getUrl(array('action' => $this->getRequest()->getActionName(), 'id' => 1)) ?>">
  <?=$this->getTokenField() ?>
-    <legend><?=$this->getTrans('manageImprint') ?></legend>
+    <legend><?=$this->getTrans('manage') ?></legend>
     <?php if($this->get('imprintStyle') == '0'): ?>
         <div class="form-group">
+            <label for="rank" class="col-lg-2 control-label">
+                <?=$this->getTrans('paragraph') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="paragraph"
                        id="paragraph"
-                       placeholder="<?php echo $this->getTrans('paragraph'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getParagraph()); } ?>" />
             </div>
         </div>
         <div class="form-group">
+            <label for="name" class="col-lg-2 control-label">
+                <?=$this->getTrans('name') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="name"
                        id="name"
-                       placeholder="<?php echo $this->getTrans('name'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getName()); } ?>" />
             </div>
         </div>
         <div class="form-group">
+            <label for="address" class="col-lg-2 control-label">
+                <?=$this->getTrans('address') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="address"
                        id="address"
-                       placeholder="<?php echo $this->getTrans('address'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getAddress()); } ?>" />
             </div>
         </div>
         <div class="form-group">
+            <label for="addressadd" class="col-lg-2 control-label">
+                <?=$this->getTrans('addressadd') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="addressadd"
                        id="addressadd"
-                       placeholder="<?php echo $this->getTrans('addressadd'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getAddressAdd()); } ?>" />
             </div>
         </div>
         <br />
         <div class="form-group">
+            <label for="city" class="col-lg-2 control-label">
+                <?=$this->getTrans('city') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="city"
                        id="city"
-                       placeholder="<?php echo $this->getTrans('city'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getCity()); } ?>" />
             </div>
         </div>
         <br />
         <div class="form-group">
+            <label for="phone" class="col-lg-2 control-label">
+                <?=$this->getTrans('phone') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="phone"
                        id="phone"
-                       placeholder="<?php echo $this->getTrans('phone'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getPhone()); } ?>" />
             </div>
         </div>
         <div class="form-group">
+            <label for="fax" class="col-lg-2 control-label">
+                <?=$this->getTrans('fax') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="fax"
                        id="fax"
-                       placeholder="<?php echo $this->getTrans('fax'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getFax()); } ?>" />
             </div>
         </div>
         <div class="form-group">
+            <label for="email" class="col-lg-2 control-label">
+                <?=$this->getTrans('email') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="email"
                        id="email"
-                       placeholder="<?php echo $this->getTrans('email'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getEmail()); } ?>" />
             </div>
         </div>
         <br />
         <div class="form-group">
+            <label for="disclaimer" class="col-lg-2 control-label">
+                <?=$this->getTrans('disclaimer') ?>:
+            </label>
             <div class="col-lg-12">
                 <textarea class="form-control"
                           id="ilch_html"
@@ -96,130 +115,157 @@
         </div>
     <?php else: ?>
         <div class="form-group">
+            <label for="paragraph" class="col-lg-2 control-label">
+                <?=$this->getTrans('paragraph') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="paragraph"
                        id="paragraph"
-                       placeholder="<?php echo $this->getTrans('paragraph'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getParagraph()); } ?>" />
             </div>
         </div>
         <div class="form-group">
+            <label for="company" class="col-lg-2 control-label">
+                <?=$this->getTrans('company') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="company"
                        id="company"
-                       placeholder="<?php echo $this->getTrans('company'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getCompany()); } ?>" />
             </div>
         </div>
         <div class="form-group">
+            <label for="name" class="col-lg-2 control-label">
+                <?=$this->getTrans('name') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="name"
                        id="name"
-                       placeholder="<?php echo $this->getTrans('name'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getName()); } ?>" />
             </div>
         </div>
         <div class="form-group">
+            <label for="address" class="col-lg-2 control-label">
+                <?=$this->getTrans('address') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="address"
                        id="address"
-                       placeholder="<?php echo $this->getTrans('address'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getAddress()); } ?>" />
             </div>
         </div>
         <div class="form-group">
+            <label for="addressadd" class="col-lg-2 control-label">
+                <?=$this->getTrans('addressadd') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="addressadd"
                        id="addressadd"
-                       placeholder="<?php echo $this->getTrans('addressadd'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getAddressAdd()); } ?>" />
             </div>
         </div>
         <br />
         <div class="form-group">
+            <label for="city" class="col-lg-2 control-label">
+                <?=$this->getTrans('city') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="city"
                        id="city"
-                       placeholder="<?php echo $this->getTrans('city'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getCity()); } ?>" />
             </div>
         </div>
         <br />
         <div class="form-group">
+            <label for="phone" class="col-lg-2 control-label">
+                <?=$this->getTrans('phone') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="phone"
                        id="phone"
-                       placeholder="<?php echo $this->getTrans('phone'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getPhone()); } ?>" />
             </div>
         </div>
         <div class="form-group">
+            <label for="fax" class="col-lg-2 control-label">
+                <?=$this->getTrans('fax') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="fax"
                        id="fax"
-                       placeholder="<?php echo $this->getTrans('fax'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getFax()); } ?>" />
             </div>
         </div>
         <div class="form-group">
+            <label for="email" class="col-lg-2 control-label">
+                <?=$this->getTrans('email') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="email"
                        id="email"
-                       placeholder="<?php echo $this->getTrans('email'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getEmail()); } ?>" />
             </div>
         </div>
         <br />
         <div class="form-group">
+            <label for="registration" class="col-lg-2 control-label">
+                <?=$this->getTrans('registration') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="registration"
                        id="registration"
-                       placeholder="<?php echo $this->getTrans('registration'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getRegistration()); } ?>" />
             </div>
         </div>
         <div class="form-group">
+            <label for="commercialregister" class="col-lg-2 control-label">
+                <?=$this->getTrans('commercialRegister') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="commercialregister"
                        id="commercialregister"
-                       placeholder="<?php echo $this->getTrans('commercialRegister'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getCommercialRegister()); } ?>" />
             </div>
         </div>
         <div class="form-group">
+            <label for="vatid" class="col-lg-2 control-label">
+                <?=$this->getTrans('vatId') ?>:
+            </label>
             <div class="col-lg-4">
                 <input class="form-control"
                        type="text"
                        name="vatid"
                        id="vatid"
-                       placeholder="<?php echo $this->getTrans('vatId'); ?>"
                        value="<?php if ($this->get('imprint') != '') { echo $this->escape($this->get('imprint')->getVatId()); } ?>" />
             </div>
         </div>
         <br />
         <div class="form-group">
+            <label for="other" class="col-lg-2 control-label">
+                <?=$this->getTrans('other') ?>:
+            </label>
             <div class="col-lg-12">
                <textarea class="form-control"
                           id="ilch_html"
@@ -230,6 +276,9 @@
         </div>
         <br />
         <div class="form-group">
+            <label for="disclaimer" class="col-lg-2 control-label">
+                <?=$this->getTrans('disclaimer') ?>:
+            </label>
             <div class="col-lg-12">
                 <textarea class="form-control"
                           id="ilch_html"
