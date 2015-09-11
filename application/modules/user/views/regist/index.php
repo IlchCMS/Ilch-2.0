240493285d6198b6ae460ac04286ed242a4c4984
JayDee <fettsack@gc-community.org>
2015-09-11 09:30:43 +0200

    fix userModul

--- application/modules/user/views/regist/index.php
+++ application/modules/user/views/regist/index.php
@@ -8,41 +8,6 @@
         </label>
         <button type="submit" name="save" class="btn pull-right"><?=$this->getTrans('nextButton') ?></button>
     </form>
-
-    <div class="omb_login">
-        <div class="row omb_row-sm-offset-3 omb_loginOr">
-            <div class="col-xs-12 col-sm-6">
-		<hr class="omb_hrOr">
-		<span class="omb_spanOr"><?=$this->getTrans('or') ?></span>
-            </div>
-	</div>
-        <div class="panel panel-info">
-            <div class="panel-heading">
-                <h3 class="panel-title"><?=$this->getTrans('menuLoginWith') ?></h3>
-            </div>
-            <div class="row omb_row-sm-offset-3 omb_socialButtons">
-                <div class="col-xs-4 col-sm-2">
-                    <a href="#" class="btn btn-lg btn-block omb_btn-facebook">
-                        <i class="fa fa-facebook visible-xs"></i>
-                        <span class="hidden-xs">Facebook</span>
-                    </a>
-                </div>
-                <div class="col-xs-4 col-sm-2">
-                    <a href="#" class="btn btn-lg btn-block omb_btn-twitter">
-                        <i class="fa fa-twitter visible-xs"></i>
-                        <span class="hidden-xs">Twitter</span>
-                    </a>
-                </div>	
-                <div class="col-xs-4 col-sm-2">
-                    <a href="#" class="btn btn-lg btn-block omb_btn-google">
-                        <i class="fa fa-google-plus visible-xs"></i>
-                        <span class="hidden-xs">Google+</span>
-                    </a>
-                </div>	
-            </div>
-        </div>
-    </div>
-
 <?php else: ?>
     Der Administrator hat festgelegt dass man sich nicht registrieren kann.
 <?php endif; ?>
