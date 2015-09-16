be5a84bc98f9b820b3007f5802cdf9df225844b2
Julian Hoen <julian.hoen@googlemail.com>
2015-09-16 18:00:52 +0200

    Update index.php

--- application/modules/guestbook/views/admin/settings/index.php
+++ application/modules/guestbook/views/admin/settings/index.php
@@ -7,9 +7,9 @@
         </label>
         <div class="col-lg-2">
             <div class="flipswitch">  
-                <input type="radio" class="flipswitch-input" name="entrySettings" value="1" id="setfree-yes" <?php if ($this->get('setfree') == '1') {echo 'checked="checked"';} ?> />  
+                <input type="radio" class="flipswitch-input" name="entrySettings" value="1" id="setfree-yes" <?php if ($this->get('setfree') == '1') { echo 'checked="checked"'; } ?> />  
                 <label for="setfree-yes" class="flipswitch-label flipswitch-label-on"><?=$this->getTrans('yes') ?></label>  
-                <input type="radio" class="flipswitch-input" name="entrySettings" value="0" id="setfree-no" <?php if ($this->get('setfree') != '1') {echo 'checked="checked"';} ?> />  
+                <input type="radio" class="flipswitch-input" name="entrySettings" value="0" id="setfree-no" <?php if ($this->get('setfree') != '1') { echo 'checked="checked"'; } ?> />  
                 <label for="setfree-no" class="flipswitch-label flipswitch-label-off"><?=$this->getTrans('no') ?></label>  
                 <span class="flipswitch-selection"></span>  
             </div>  
