4df5554d0d7be511a0fbc46a96321d88844f3ec6
Thomas Stantin <th.stantin@gmx.de>
2016-01-20 08:24:16 +0100

    delete unused files

--- application/libraries/Captcha/Captcha.php
+++ application/libraries/Captcha/Captcha.php
@@ -32,7 +32,7 @@ class Captcha
     public $height = 70;
 
     /** Dictionary word file (empty for random text) */
-    public $wordsFile = 'words/en.php';
+    public $wordsFile = '';
 
     /**
      * Path for resource files (fonts, words, etc.)
