713cbb0e731450e0c64826aee1ad420630aff4fa
Thomas Stantin <th.stantin@gmx.de>
2016-01-24 11:47:30 +0100

    Fix redirect after login from self pages

--- application/modules/user/boxes/Login.php
+++ application/modules/user/boxes/Login.php
@@ -12,7 +12,7 @@ class Login extends \Ilch\Box
     {
         $this->getView()->setArray([
             'regist_accept' => $this->getConfig()->get('regist_accept'),
-            'redirectUrl' => $this->getView()->getCurrentUrl()
+            'redirectUrl' => $this->getRouter()->getQuery()
         ]);
     }
 }
