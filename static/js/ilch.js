8f40d01cd91bf7204d131ed2ff1c5175fff111e4
Thomas Stantin <th.stantin@gmx.de>
2016-03-31 00:49:17 +0200

    Add Bootstrap to CkEditor

--- static/js/ilch.js
+++ static/js/ilch.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
             CKEDITOR.replace( id , {
                 removePlugins: 'bbcode',
                 disableObjectResizing: false,
+                contentsCss: [CKEDITOR.basePath + '../../css/bootstrap.css'],
                 allowedContent: true,
                 toolbar : toolbar
             });
