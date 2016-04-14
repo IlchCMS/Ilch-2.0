caef6ec90884267e2c500960a4e420cc5bf9e143
Thomas Stantin <th.stantin@gmx.de>
2016-04-14 13:11:12 +0200

    Fix Module Install ajaxconfig

--- application/modules/install/views/index/ajaxconfig.php
+++ application/modules/install/views/index/ajaxconfig.php
@@ -1,46 +1,31 @@
-<?php
-$i = 0;
-$modulesToInstall = $this->get('modulesToInstall');
-?>
+<?php $modulesToInstall = $this->get('modulesToInstall'); ?>
+<br><i><?=$this->getTrans('obligatoryModules') ?></i><br><br>
+<div class="row">
+    <?php foreach ($this->get('modules') as $key => $module): ?>
+        <?php if(isset($module['config']->config['system_module'])): ?>
 
-<?php foreach ($this->get('modules') as $key => $module): ?>
-    <?php if ($i == 0): ?>
-        <i><?=$this->getTrans('obligatoryModules') ?></i><br /><br />
-    <?php endif; ?>
-
-    <?php if ($i !== 0 && $i % 9 == 0): ?>
-        </div>
-    <?php endif; ?>
-
-    <?php if ($i == 9): ?>
-        <br /><i><?=$this->getTrans('optionalModules') ?></i><br /><br />
-    <?php endif; ?>
-
-    <?php if ($i % 9 == 0): ?>
-        <div class="row">
-    <?php endif; ?>
-
-    <div class="col-lg-4">
-        <input id="module_<?=$key ?>" <?php if(isset($module['config']->config['system_module'])) { echo 'disabled="disabled"';} ?>
-            <?php if(
-                        isset($module['config']->config['system_module'])
-                    ||
-                        (
-                            !empty($modulesToInstall)
-                            && in_array($key, $modulesToInstall)
-                        )
-                    ||
-                        (
-                            empty($modulesToInstall)
-                            && (isset($module['checked']))
-                        ))
-                { echo 'checked="checked"'; } ?>
-               type="checkbox"
-               name="modulesToInstall[]"
-               value="<?=$key ?>" />
-        <label for="module_<?=$key ?>">
-            <?=$module['config']->config['languages'][$this->getTranslator()->getLocale()]['name']; ?>
-        </label>
-    </div>
-    <?php $i++; ?>
-<?php endforeach; ?>
+                <div class="col-lg-4">
+                    <input id="module_<?=$key ?>" disabled="disabled" checked="checked" name="modulesToInstall[]" value="<?=$key ?>" type="checkbox">
+                    <label for="module_<?=$key ?>">
+                        <?=$module['config']->config['languages'][$this->getTranslator()->getLocale()]['name']; ?>
+                    </label>
+                </div>
+        <?php endif; ?>
+    <?php endforeach; ?>
+</div>
+<br><i><?=$this->getTrans('optionalModules') ?></i><br><br>
+<div class="row">
+    <?php foreach ($this->get('modules') as $key => $module): ?>
+        <?php if(!isset($module['config']->config['system_module'])): ?>
+            <div class="col-lg-4">
+                <input id="module_<?=$key ?>" name="modulesToInstall[]" value="<?=$key ?>" type="checkbox"
+                    <?php if (isset($module['checked'])) {
+                        echo 'checked="checked"';
+                    } ?>>
+                <label for="module_<?=$key ?>">
+                    <?=$module['config']->config['languages'][$this->getTranslator()->getLocale()]['name']; ?>
+                </label>
+            </div>
+        <?php endif; ?>
+    <?php endforeach; ?>
+</div>
