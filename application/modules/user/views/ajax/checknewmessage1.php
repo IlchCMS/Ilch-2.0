e23c1cf0f537e6ec4802861423fcee193967c65e
Thomas Stantin <th.stantin@gmx.de>
2016-05-15 09:02:19 +0200

    rename fix

--- application/modules/user/views/ajax/checknewmessage1.php
+++ application/modules/user/views/ajax/checknewmessage1.php
@@ -3,3 +3,4 @@ if ($this->get('dialogUnread') != '') {
     $dialogUnread = $this->get('dialogUnread');
     echo '<a href="'.$this->getUrl(array('module' => 'user', 'controller' => 'panel', 'action' => 'dialog')).'" style="color:red;">Neu (<i class="fa fa-envelope-o"></i>)</a>';
 }
+?>
