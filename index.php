edc7603c2505607f2fba1a334db057a0d9bbcb98
blackcoder87 <blackcoder@gmx.de>
2016-05-16 12:33:31 +0200

    Move re-authentication-code to Ilch\Page()
    Moved the code to Ilch\Page(), which made it easy to call redirect().
    This fixes the issue that a successfull login was only visible after
    manually refreshing the page.

--- index.php
+++ index.php
@@ -46,39 +46,6 @@ $loader->registNamespace('Thumb');
 try {
     $page = new \Ilch\Page();
     $page->loadCms();
-
-    if (empty($_SESSION['user_id']) && !empty($_COOKIE['remember'])) {
-        list($selector, $authenticator) = explode(':', $_COOKIE['remember']);
-
-        $authTokenMapper = new Modules\User\Mappers\AuthToken();
-        $row = $authTokenMapper->getAuthToken($selector);
-
-        if(!empty($row) && strtotime($row['expires']) >= time()) {
-            if (hash_equals($row['token'], hash('sha256', base64_decode($authenticator)))) {
-                $_SESSION['user_id'] = $row['userid'];
-                // A new token is generated, a new hash for the token is stored over the old record, and a new login cookie is issued to the user.
-                $authTokenModel = new Modules\User\Models\AuthToken();
-
-                // 9 bytes of random data (base64 encoded to 12 characters) for the selector.
-                // This provides 72 bits of keyspace and therefore 236 bits of collision resistance (birthday attacks)
-                $authTokenModel->setSelector(base64_encode(openssl_random_pseudo_bytes(9)));
-                // 33 bytes (264 bits) of randomness for the actual authenticator. This should be unpredictable in all practical scenarios.
-                $authenticator = openssl_random_pseudo_bytes(33);
-                // SHA256 hash of the authenticator. This mitigates the risk of user impersonation following information leaks.
-                $authTokenModel->setToken(hash('sha256', $authenticator));
-                $authTokenModel->setUserid($_SESSION['user_id']);
-                $authTokenModel->setExpires(date('Y-m-d\TH:i:s', strtotime( '+30 days' )));
-
-                setcookie('remember', $authTokenModel->getSelector().':'.base64_encode($authenticator), strtotime( '+30 days' ), '/', $_SERVER['SERVER_NAME'], false, false);
-                $authTokenMapper->updateAuthToken($authTokenModel);
-            } else {
-                // If the series is present but the token does not match, a theft is assumed.
-                // The user receives a strongly worded warning and all of the user's remembered sessions are deleted.
-                $authTokenMapper->deleteAllAuthTokenOfUser($row['userid']);
-            }
-        }
-    }
-
     $page->loadPage();
 } catch (Exception $ex) {
     print 'An unexpected error occurred: ' . $ex->getMessage();
