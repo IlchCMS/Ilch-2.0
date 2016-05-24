4ad776c3e5a9aac1648b8403f55b9393605b77d6
blackcoder87 <blackcoder@gmx.de>
2016-05-24 19:06:56 +0200

    Renamed variables and added comments

--- application/modules/media/views/admin/index/upload.php
+++ application/modules/media/views/admin/index/upload.php
@@ -51,8 +51,8 @@ function toJavaScriptArray($array) {
 <script language="javascript">
     const allowedExtensions = <?=toJavaScriptArray(explode(' ',$this->get('media_ext_img') . $this->get('media_ext_file') . $this->get('media_ext_video')));?>;
     var maxFileSize = <?=return_bytes(ini_get('upload_max_filesize'));?>;
-    var messageFileTooBig = '<?=$this->getTrans('fileTooBig');?>';
-    var messageExtensionNotAllowed = '<?=$this->getTrans('extensionNotAllowed');?>';
+    var fileTooBig = '<?=$this->getTrans('fileTooBig');?>';
+    var extensionNotAllowed = '<?=$this->getTrans('extensionNotAllowed');?>';
    $(document).ready(function(){
 	$("[rel='tooltip']").tooltip();
     });
