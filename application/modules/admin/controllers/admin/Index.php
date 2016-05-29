be2b3c137ff0ca1ebfa2d5b1654560fefceded8c
blackcoder87 <blackcoder@gmx.de>
2016-05-30 01:34:43 +0200

    Remove unneeded code

--- application/modules/admin/controllers/admin/Index.php
+++ application/modules/admin/controllers/admin/Index.php
@@ -20,20 +20,15 @@ class Index extends \Ilch\Controller\Admin
         $authTokenMapper->deleteExpiredAuthTokens();
 
         // Check if Ilch is up to date
-        $doUpdate = $this->getRequest()->getParam('doupdate');
-        $doSave = $this->getRequest()->getParam('dosave');
         $version = $this->getConfig()->get('version');
-        $this->getView()->set('version', $version);
 
         $update = new \Ilch\Transfer();
         $update->setTransferUrl($this->getConfig()->get('master_update_url'));
         $update->setVersionNow($version);
         $update->setCurlOpt(CURLOPT_RETURNTRANSFER, 1);
         $update->setCurlOpt(CURLOPT_FAILONERROR, true);
-        $update->setZipSavePath(ROOT_PATH.'/updates/');
 
-        $result = $update->getVersions();
-        if ($result == '') {
+        if ($update->getVersions() == '') {
             $this->addMessage(curl_error($update->getTransferUrl()), 'danger');
         }
 
