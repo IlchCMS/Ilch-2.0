a0796b07a247f4183c15f1a03b3cf0c4fd88c9d8
blackcoder87 <blackcoder@gmx.de>
2016-07-06 21:26:40 +0200

    Add setting to article-module

---
+++ application/modules/article/views/admin/settings/index.php
@@ -0,0 +1,18 @@
+<form class="form-horizontal" method="POST" action="">
+    <?=$this->getTokenField() ?>
+    <legend><?=$this->getTrans('settings') ?></legend>
+    <div class="form-group">
+        <label for="articlesPerPageInput" class="col-lg-2 control-label">
+            <?=$this->getTrans('articlePerPage') ?>:
+        </label>
+        <div class="col-lg-8">
+            <input class="form-control"
+                   id="articlesPerPageInput"
+                   name="articlesPerPage"
+                   type="number"
+                   min="1"
+                   value="<?=$this->escape($this->get('articlesPerPage')) ?>" />
+        </div>
+    </div>
+    <?=$this->getSaveBar() ?>
+</form>
