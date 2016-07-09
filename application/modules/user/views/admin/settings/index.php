dbfb7d910baa39a72eb7a2625d70882d9123b763
blackcoder87 <blackcoder@gmx.de>
2016-07-09 17:06:58 +0200

    Add setting to user-module

--- application/modules/user/views/admin/settings/index.php
+++ application/modules/user/views/admin/settings/index.php
@@ -187,6 +187,19 @@
                    value="<?=$this->get('usergallery_filetypes') ?>" />
         </div>
     </div>
+    <div class="form-group">
+        <label for="picturesPerPageInput" class="col-lg-2 control-label">
+            <?=$this->getTrans('picturesPerPage') ?>:
+        </label>
+        <div class="col-lg-2">
+            <input class="form-control"
+                   id="picturesPerPageInput"
+                   name="picturesPerPage"
+                   type="number"
+                   min="1"
+                   value="<?=$this->escape($this->get('picturesPerPage')) ?>" />
+        </div>
+    </div>
     <?=$this->getSaveBar() ?>
 </form>
 
