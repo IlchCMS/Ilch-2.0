81205137b688d32f82a57fb540890a666eda76d6
kveldscholten <kevin_veldscholten@online.de>
2016-07-24 13:30:46 +0200

    Fix Events Module
    fix date and change show to list

--- application/modules/events/boxes/views/events.php
+++ application/modules/events/boxes/views/events.php
@@ -1,11 +1,17 @@
 <?php if ($this->get('eventList') != ''): ?>
-    <?php foreach ($this->get('eventList') as $eventlist): ?>
-        <?php $date = new \Ilch\Date($eventlist->getDateCreated()); ?>
-        <i class="fa fa-calendar"></i> 
-        <a href="<?=$this->getUrl('events/index/show/id/' . $eventlist->getId()) ?>"><?=((strlen($this->escape($eventlist->getTitle()))<15) ? $this->escape($eventlist->getTitle()) : substr($this->escape($eventlist->getTitle()),0,15).'...') ?></a> <span class="small" style="float: right;">(<?=$date->format("d.m", true) ?>)</span><br />
-    <?php endforeach; ?>
+    <ul class="list-unstyled">
+        <?php foreach ($this->get('eventList') as $eventlist): ?>
+            <?php $date = new \Ilch\Date($eventlist->getStart()); ?>
+            <li>
+                <i class="fa fa-calendar"></i>
+                <a href="<?=$this->getUrl('events/show/event/id/' . $eventlist->getId()) ?>">
+                    <?=((strlen($this->escape($eventlist->getTitle()))<15) ? $this->escape($eventlist->getTitle()) : substr($this->escape($eventlist->getTitle()),0,15).'...') ?>
+                </a>
+                <span class="small pull-right"><?=$date->format("d.m", true) ?></span>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 <?php else: ?>
     <?=$this->getTrans('noEvent') ?>
 <?php endif; ?>
-<hr />
 <div align="center"><a href="<?=$this->getUrl('events/index/index/') ?>"><?=$this->getTrans('otherEvents') ?></a></div>
