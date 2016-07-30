6fcbefe4b124dc68cb28853cf7741847b3a884bb
kveldscholten <kevin_veldscholten@online.de>
2016-07-30 02:40:38 +0200

    Fix install module

--- application/modules/install/views/index/license.php
+++ application/modules/install/views/index/license.php
@@ -1,6 +1,6 @@
 <div class="form-group <?php if ($this->get('error') != '') { echo 'has-error'; } ?>">
     <div class="col-lg-12">
-        <?php if (!empty($this->get('licenseMissing'))): ?>
+        <?php if ($this->get('licenseMissing') != ''): ?>
             <div class="alert alert-danger" role="alert">
                 <?=$this->getTrans($this->get('licenseMissing')) ?>
             </div>
@@ -10,7 +10,7 @@
             </div>
         <?php endif; ?>
     </div>
-    <?php if (empty($this->get('licenseMissing'))): ?>
+    <?php if ($this->get('licenseMissing') == ''): ?>
         <label class="col-lg-12 checkbox inline <?php if ($this->get('error') != '') { echo 'text-danger'; } ?>" style="margin-left: 20px;">
             <input type="checkbox" name="licenseAccepted" value="1" <?php if ($this->get('licenseAccepted') != '') { echo 'checked'; } ?>> <?=$this->getTrans('acceptLicense') ?>
         </label>
