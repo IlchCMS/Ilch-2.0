9d6661fc225c14232c8cdc59de68fbd0b3f6afde
kveldscholten <kevin_veldscholten@online.de>
2016-08-02 18:56:02 +0200

    Add table-responsive to User Module

--- application/modules/user/views/admin/index/index.php
+++ application/modules/user/views/admin/index/index.php
@@ -15,83 +15,85 @@
         <?php endif; ?>
     </ul>
     <br />
-    <table class="table table-hover table-striped">
-        <colgroup>
-            <col class="icon_width" />
-            <col class="icon_width">
-            <col class="icon_width" />
-            <col class="col-lg-2" />
-            <col class="col-lg-2" />
-            <col class="col-lg-2" />
-            <col />
-        </colgroup>
-        <thead>
-            <tr>
-                <th><?=$this->getCheckAllCheckbox('check_users') ?></th>
-                <th></th>
-                <th></th>
-                <th><?=$this->getTrans('userName') ?></th>
-                <th><?=$this->getTrans('userEmail') ?></th>
-                <th><?=$this->getTrans('userDateCreated') ?></th>
-                <th><?=$this->getTrans('userGroups') ?></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            if ($this->get('userList') != ''):
-                foreach ($this->get('userList') as $user):
-                    $groups = '';
+    <div class="table-responsive">
+        <table class="table table-hover table-striped">
+            <colgroup>
+                <col class="icon_width" />
+                <col class="icon_width">
+                <col class="icon_width" />
+                <col class="col-lg-2" />
+                <col class="col-lg-2" />
+                <col class="col-lg-2" />
+                <col />
+            </colgroup>
+            <thead>
+                <tr>
+                    <th><?=$this->getCheckAllCheckbox('check_users') ?></th>
+                    <th></th>
+                    <th></th>
+                    <th><?=$this->getTrans('userName') ?></th>
+                    <th><?=$this->getTrans('userEmail') ?></th>
+                    <th><?=$this->getTrans('userDateCreated') ?></th>
+                    <th><?=$this->getTrans('userGroups') ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if ($this->get('userList') != ''):
+                    foreach ($this->get('userList') as $user):
+                        $groups = '';
 
-                    foreach ($user->getGroups() as $group) {
-                        if ($groups != '') {
-                            $groups .= ', ';
+                        foreach ($user->getGroups() as $group) {
+                            if ($groups != '') {
+                                $groups .= ', ';
+                            }
+
+                            $groups .= $group->getName();
                         }
 
-                        $groups .= $group->getName();
-                    }
+                        if ($groups === '') {
+                            $groups = $this->getTrans('noGroupsAssigned');
+                        }
 
-                    if ($groups === '') {
-                        $groups = $this->getTrans('noGroupsAssigned');
-                    }
+                        $dateConfirmed = $user->getDateConfirmed();
 
-                    $dateConfirmed = $user->getDateConfirmed();
+                        if ($dateConfirmed == '') {
+                            $dateConfirmed = $this->getTrans('notConfirmedYet');
+                        }
 
-                    if ($dateConfirmed == '') {
-                        $dateConfirmed = $this->getTrans('notConfirmedYet');
-                    }
+                        $dateLastActivity = $user->getDateLastActivity();
 
-                    $dateLastActivity = $user->getDateLastActivity();
-
-                    if ($dateLastActivity !== null && $dateLastActivity->getTimestamp() == 0) {
-                        $dateLastActivity = $this->getTrans('neverLoggedIn');
-                    }
-                    ?>
+                        if ($dateLastActivity !== null && $dateLastActivity->getTimestamp() == 0) {
+                            $dateLastActivity = $this->getTrans('neverLoggedIn');
+                        }
+                        ?>
+                        <tr>
+                            <td>
+                                <input value="<?=$user->getId()?>" type="checkbox" name="check_users[]" />
+                            </td>
+                            <td>
+                                <?php  if ($this->getRequest()->getParam('showsetfree')): ?>
+                                    <a href="<?=$this->getUrl(['action' => 'setfree', 'id' => $user->getId()]) ?>" title="<?=$this->getTrans('setfree') ?>"><i class="fa fa-check text-success"></i></a>
+                                <?php else: ?>                                
+                                    <?=$this->getEditIcon(['action' => 'treat', 'id' => $user->getId()]) ?>
+                                <?php endif; ?>
+                            </td>
+                            <td>
+                                <?=$this->getDeleteIcon(['action' => 'delete', 'id' => $user->getId()]) ?>
+                            </td>
+                            <td><?=$this->escape($user->getName()) ?></td>
+                            <td><?=$this->escape($user->getEmail()) ?></td>
+                            <td><?=$this->escape($user->getDateCreated()) ?></td>
+                            <td><?=$this->escape($groups) ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else: ?>
                     <tr>
-                        <td>
-                            <input value="<?=$user->getId()?>" type="checkbox" name="check_users[]" />
-                        </td>
-                        <td>
-                            <?php  if ($this->getRequest()->getParam('showsetfree')): ?>
-                                <a href="<?=$this->getUrl(['action' => 'setfree', 'id' => $user->getId()]) ?>" title="<?=$this->getTrans('setfree') ?>"><i class="fa fa-check text-success"></i></a>
-                            <?php else: ?>                                
-                                <?=$this->getEditIcon(['action' => 'treat', 'id' => $user->getId()]) ?>
-                            <?php endif; ?>
-                        </td>
-                        <td>
-                            <?=$this->getDeleteIcon(['action' => 'delete', 'id' => $user->getId()]) ?>
-                        </td>
-                        <td><?=$this->escape($user->getName()) ?></td>
-                        <td><?=$this->escape($user->getEmail()) ?></td>
-                        <td><?=$this->escape($user->getDateCreated()) ?></td>
-                        <td><?=$this->escape($groups) ?></td>
+                        <td colspan="7"><?=$this->getTrans('noUsersExist') ?></td>
                     </tr>
-                <?php endforeach; ?>
-            <?php else: ?>
-                <tr>
-                    <td colspan="7"><?=$this->getTrans('noUsersExist') ?></td>
-                </tr>
-            <?php endif; ?>
-        </tbody>
-    </table>
+                <?php endif; ?>
+            </tbody>
+        </table>
+    </div>
     <?=$this->getListBar(['delete' => 'delete']) ?>
 </form>
