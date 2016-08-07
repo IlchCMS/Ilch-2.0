c3cc732ac8c27487686b96f475810014eb3453fe
blackcoder87 <blackcoder@gmx.de>
2016-08-07 13:04:01 +0200

    Add missing translation for guestbook-module

--- application/modules/guestbook/translations/en.php
+++ application/modules/guestbook/translations/en.php
@@ -37,4 +37,13 @@ return [
     'smilies' => 'Smilies',
     'close' => 'Close',
     'entriesPerPage' => 'Einträge pro Seite',
+
+    'validation.errors.required.fieldIsRequired'    => '%s was not filled out.',
+    'validation.errors.same.fieldsDontMatch'        => '%s must be equal %s.',
+    'validation.errors.captcha.wrongCaptcha'        => '%s is the wrong character combination.',
+    'validation.errors.length.tooShort'             => '%s must be at least %s chars long.',
+    'validation.errors.length.tooLong'              => '%s has to be a maximum of %s chars long.',
+    'validation.errors.length.tooShortAndOrTooLong' => '%s must be at least %s chars long and maximum %s chars long.',
+    'validation.errors.url.noValidUrl'              => '%s must be a valid URL.',
+    'validation.errors.email.noValidEmail'          => '%s must be a valid email address.',
 ];
