4e56b846fbbb9ce7796d0da8ee78cdb3cb5d8572
dastiii <github@tobias-schwarz.me>
2016-08-07 18:41:29 +0200

    Validation: Adds default error messages to the core

--- application/libraries/Ilch/Translations/de.php
+++ application/libraries/Ilch/Translations/de.php
@@ -5,5 +5,12 @@
  */
 
 return [
-    // core translations
+    'validation.errors.required.fieldIsRequired'    => '%s muss ausgefüllt werden.',
+    'validation.errors.same.fieldsDontMatch'        => '%s muss mit %s übereinstimmen.',
+    'validation.errors.captcha.wrongCaptcha'        => '%s entspricht nicht der gezeigten Zeichenkombination',
+    'validation.errors.length.tooShort'             => '%s muss mindestens %s Zeichen lang sein.',
+    'validation.errors.length.tooLong'              => '%s darf höchstens %s Zeichen lang sein.',
+    'validation.errors.length.tooShortAndOrTooLong' => '%s muss mindestens %s und darf höchstens %s Zeichen lang sein',
+    'validation.errors.url.noValidUrl'              => '%s muss eine gültige URL sein.',
+    'validation.errors.email.noValidEmail'          => '%s muss eine gültige E-Mail-Adresse sein.',
 ];
