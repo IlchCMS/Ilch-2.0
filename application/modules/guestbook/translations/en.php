78a6c1e11ba7304c95a5c14aaa588bd3cb837e1c
Tobias Schwarz <github@tobias-schwarz.me>
2016-08-07 18:45:07 +0200

    Translations: Removes duplicate validation messages

--- application/modules/guestbook/translations/en.php
+++ application/modules/guestbook/translations/en.php
@@ -38,13 +38,4 @@ return [
     'close' => 'Close',
     'entriesPerPage' => 'Einträge pro Seite',
     'errorsOccured' => 'The following errors occured',
-
-    'validation.errors.required.fieldIsRequired'    => '%s was not filled out.',
-    'validation.errors.same.fieldsDontMatch'        => '%s must be equal %s.',
-    'validation.errors.captcha.wrongCaptcha'        => '%s is the wrong character combination.',
-    'validation.errors.length.tooShort'             => '%s must be at least %s chars long.',
-    'validation.errors.length.tooLong'              => '%s has to be a maximum of %s chars long.',
-    'validation.errors.length.tooShortAndOrTooLong' => '%s must be at least %s chars long and maximum %s chars long.',
-    'validation.errors.url.noValidUrl'              => '%s must be a valid URL.',
-    'validation.errors.email.noValidEmail'          => '%s must be a valid email address.',
 ];
