320c321c19c4112619d397574f33beab4b45dcff
blackcoder87 <blackcoder@gmx.de>
2016-09-18 09:19:08 +0200

    Renamed variables and adjusted condition

--- application/modules/admin/views/admin/modules/search.php
+++ application/modules/admin/views/admin/modules/search.php
@@ -1,6 +1,6 @@
 <?php
 $modulesList = url_get_contents('http://ilch2.de/downloads/modules/list.php');
-$modules = json_decode($modulesList);
+$modulesOnUpdateServer = json_decode($modulesList);
 $versionsOfModules = $this->get('versionsOfModules');
 ?>
 
@@ -8,7 +8,7 @@ $versionsOfModules = $this->get('versionsOfModules');
 
 <legend><?=$this->getTrans('search') ?></legend>
 <?php
-if (empty($modules)) {
+if (empty($modulesOnUpdateServer)) {
     echo $this->getTrans('noModulesAvailable');
     return;
 }
@@ -27,15 +27,15 @@ if (empty($modules)) {
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($modules as $module):  ?>
+            <?php foreach ($modulesOnUpdateServer as $moduleOnUpdateServer):  ?>
                 <?php
-                if (!empty($module->phpextensions)) {
+                if (!empty($moduleOnUpdateServer->phpextensions)) {
                     $extensionCheck = [];
-                    foreach ($module->phpextensions as $extension) {
+                    foreach ($moduleOnUpdateServer->phpextensions as $extension) {
                         $extensionCheck[] = extension_loaded($extension);
                     }
 
-                    $phpExtensions = array_combine($module->phpextensions, $extensionCheck);
+                    $phpExtensions = array_combine($moduleOnUpdateServer->phpextensions, $extensionCheck);
                     foreach ($phpExtensions as $key => $value) {
                         if ($value == true) {
                             $phpExtension[] = '<font color="#3c763d">'.$key.'</font>';
@@ -49,37 +49,37 @@ if (empty($modules)) {
                 ?>
                 <tr>
                     <td>
-                        <a href="<?=$this->getUrl(['action' => 'show', 'id' => $module->id]); ?>" title="<?=$this->getTrans('info') ?>"><?=$this->escape($module->name) ?></a>
+                        <a href="<?=$this->getUrl(['action' => 'show', 'id' => $moduleOnUpdateServer->id]); ?>" title="<?=$this->getTrans('info') ?>"><?=$this->escape($moduleOnUpdateServer->name) ?></a>
                         <br />
                         <small>
                             <?=$this->getTrans('author') ?>: 
-                            <?php if ($module->link != ''): ?>
-                                <a href="<?=$module->link ?>" alt="<?=$this->escape($module->author) ?>" title="<?=$this->escape($module->author) ?>" target="_blank"><i><?=$this->escape($module->author) ?></i></a>
+                            <?php if ($moduleOnUpdateServer->link != ''): ?>
+                                <a href="<?=$moduleOnUpdateServer->link ?>" alt="<?=$this->escape($moduleOnUpdateServer->author) ?>" title="<?=$this->escape($moduleOnUpdateServer->author) ?>" target="_blank"><i><?=$this->escape($moduleOnUpdateServer->author) ?></i></a>
                             <?php else: ?>
-                                <i><?=$this->escape($module->author) ?></i>
+                                <i><?=$this->escape($moduleOnUpdateServer->author) ?></i>
                             <?php endif; ?>
                         </small>
                         <br /><br />
                         <?php
-                        $filename = basename($module->downloadLink);
+                        $filename = basename($moduleOnUpdateServer->downloadLink);
                         $filename = strstr($filename,'.',true);
-                        if (!empty($module->phpextensions) AND in_array(false, $extensionCheck)): ?>
+                        if (!empty($moduleOnUpdateServer->phpextensions) AND in_array(false, $extensionCheck)): ?>
                             <button class="btn disabled"
                                     title="<?=$this->getTrans('phpExtensionError') ?>">
                                 <i class="fa fa-download"></i>
                             </button>
-                        <?php elseif (in_array($filename, $this->get('modules')) && $module->version == $versionsOfModules[$module->key]['version']): ?>
+                        <?php elseif (in_array($filename, $this->get('modules')) && $moduleOnUpdateServer->version <= $versionsOfModules[$moduleOnUpdateServer->key]['version']): ?>
                             <button class="btn disabled"
                                     title="<?=$this->getTrans('alreadyExists') ?>">
                                 <i class="fa fa-check text-success"></i>
                             </button>
-                        <?php elseif (in_array($filename, $this->get('modules')) && $module->version > $versionsOfModules[$module->key]['version']): ?>
+                        <?php elseif (in_array($filename, $this->get('modules')) && $moduleOnUpdateServer->version > $versionsOfModules[$moduleOnUpdateServer->key]['version']): ?>
                             <form method="POST" action="<?=$this->getUrl(['action' => 'update']) ?>">
                                 <?=$this->getTokenField() ?>
                                 <button type="submit"
                                         class="btn btn-default"
                                         name="url"
-                                        value="<?=$module->downloadLink ?>"
+                                        value="<?=$moduleOnUpdateServer->downloadLink ?>"
                                         title="<?=$this->getTrans('moduleUpdate') ?>">
                                     <i class="fa fa-refresh"></i>
                                 </button>
@@ -90,20 +90,20 @@ if (empty($modules)) {
                                 <button type="submit"
                                         class="btn btn-default"
                                         name="url"
-                                        value="<?=$module->downloadLink ?>"
+                                        value="<?=$moduleOnUpdateServer->downloadLink ?>"
                                         title="<?=$this->getTrans('moduleDownload') ?>">
                                     <i class="fa fa-download"></i>
                                 </button>
                             </form>
                         <?php endif; ?>
 
-                        <a href="<?=$this->getUrl(['action' => 'show', 'id' => $module->id]); ?>" title="<?=$this->getTrans('info') ?>">
+                        <a href="<?=$this->getUrl(['action' => 'show', 'id' => $moduleOnUpdateServer->id]); ?>" title="<?=$this->getTrans('info') ?>">
                             <span class="btn btn-default">
                                 <i class="fa fa-info text-info"></i>
                             </span>
                         </a>
                     </td>
-                    <td><?=$module->desc ?></td>
+                    <td><?=$moduleOnUpdateServer->desc ?></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
