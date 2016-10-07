c51613dc979a30942d266eb550a6d59e3f5cd089
Thomas Stantin <th.stantin@gmx.de>
2016-10-07 06:59:02 +0200

    Add month name to default translations

--- application/libraries/Ilch/Translations/en.php
+++ application/libraries/Ilch/Translations/en.php
@@ -36,4 +36,18 @@ return [
     // comment module
     'iLike' => 'I like this',
     'notLike' => 'I dislike this',
+
+    // month name
+    'January' => 'January',
+    'February' => 'February',
+    'March' => 'March',
+    'April' => 'April',
+    'May' => 'May',
+    'June' => 'June',
+    'July' => 'July',
+    'August' => 'August',
+    'September' => 'September',
+    'October' => 'October',
+    'November' => 'November',
+    'December' => 'December',
 ];
