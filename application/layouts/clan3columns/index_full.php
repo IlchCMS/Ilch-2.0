042eaa9ca96189189d26fcc8d5e447864dc91d02
kveldscholten <kevin_veldscholten@online.de>
2016-10-16 19:57:32 +0200

    Update clan3columns layout

--- application/layouts/clan3columns/index_full.php
+++ application/layouts/clan3columns/index_full.php
@@ -9,27 +9,116 @@
         <script type="text/javascript" src="<?=$this->getVendorUrl('twbs/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
     </head>
     <body>
-        <div id="main">
-            <div class="row">
-                <div class="col-lg-2">
-                    <?php
-                    echo $this->getMenu
-                    (
-                        1,
-                        '<div class="panel panel-gaming">
-                             <div class="panel-heading">%s</div>
-                                <div class="panel-body">
-                                    %c
-                                </div>
-                         </div>'
-                    );
-                    ?>
+        <header>
+            <nav class="navbar navbar-gaming navbar-fixed-top hidden-sm hidden-md hidden-lg">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse-main" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="/">Navigation</a>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="bs-navbar-collapse-main">
+                        <?php
+                        echo $this->getMenu
+                        (
+                            1,
+                            '<div class="panel panel-gaming">
+                                     <div class="panel-heading">%s</div>
+                                        <div class="panel-body">
+                                            %c
+                                        </div>
+                                 </div>'
+                        );
+                        ?>
+                    </div>
                 </div>
-                <div class="col-lg-10">
-                    <?=$this->getHmenu() ?>
-                    <div class="panel panel-default" id="headings">
-                        <div class="panel-body">
-                            <?=$this->getContent() ?>
+            </nav>
+
+            <div class="container">
+                <div class="gaming">
+                    <div id="carousel-generic" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carousel-generic" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-generic" data-slide-to="2"></li>
+                        </ol>
+
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img src="<?=$this->getLayoutUrl('img/slider/slider_1.jpg') ?>" alt="Slider 1">
+                            </div>
+                            <div class="item">
+                                <img src="<?=$this->getLayoutUrl('img/slider/slider_2.jpg') ?>" alt="Slider 2">
+                            </div>
+                            <div class="item">
+                                <img src="<?=$this->getLayoutUrl('img/slider/slider_3.jpg') ?>" alt="Slider 2">
+                            </div>
+                        </div>
+
+                        <a class="left carousel-control" href="#carousel-generic" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-generic" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+
+                    <div class="gaming-name">
+                        Clanname
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <div class="container">
+            <div class="gaming">
+                <div class="row">
+                    <div class="hidden-xs col-sm-3 col-md-2">
+                        <?php
+                        echo $this->getMenu
+                        (
+                            1,
+                            '<div class="panel panel-gaming">
+                                 <div class="panel-heading">%s</div>
+                                    <div class="panel-body">
+                                        %c
+                                    </div>
+                             </div>'
+                        );
+                        ?>
+                    </div>
+                    <div class="col-xs-12 col-sm-9 col-md-10">
+                        <?=$this->getHmenu() ?>
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <?=$this->getContent() ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer">
+            <div class="container">
+                <div class="gaming">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6">
+                            &copy; Ilch 2.0 Free Layout | CMS by <a href="http://www.ilch.de/">Ilch</a>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 nav">
+                            <ul>
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/index.php/contact/index/index">Kontakt</a></li>
+                                <li><a href="/index.php/imprint/index/index">Impressum</a></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
@@ -37,12 +126,5 @@
         </div>
 
         <?=$this->getFooter() ?>
-        <script type='text/javascript'>
-        $(document).ready(function() {
-             $('.carousel').carousel({
-                 interval: 4000
-             });
-        }); 
-        </script>
     </body>
 </html>
