087c220a1b78236f85b23935ff6230e200b1f0b1
Stefan Gräwe <stefan.graewe@gmail.com>
2016-10-18 14:55:53 +0200

    Update clan3columns layout description

--- application/layouts/clan3columns/config/config.php
+++ application/layouts/clan3columns/config/config.php
@@ -9,7 +9,7 @@ class Config extends \Ilch\Config\Install
         'version' => '1.0',
         'author' => 'Ilch.de',
         'link' => 'http://ilch.de',
-        'desc' => '2 Spalten Clan Layout',
+        'desc' => '3 Spalten Clan Layout',
         'layouts' => [
             'index_full' => [
                 ['module' => 'user', 'controller' => 'panel'],
