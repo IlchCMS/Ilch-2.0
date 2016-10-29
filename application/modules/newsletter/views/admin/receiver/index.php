3ed605ac567edc64f32ff11dbcc6a35bc5287dc0
blackcoder87 <blackcoder@gmx.de>
2016-10-29 18:45:26 +0200

    Add deleting of subscribers
    Subscribers/Receivers can now be deleted in the admincenter.
    
    Added deleteSubscriberBySelector() to avoid adding an email address as
    parameter to the deleteAction-url. Instead of an email-address the
    selector is now added as parameter.
    
    Email addresses or other potential sensible data should not be added as
    parameters in urls to avoid them being leaked in browser histories or to
    proxies.

--- application/modules/newsletter/views/admin/receiver/index.php
+++ application/modules/newsletter/views/admin/receiver/index.php
@@ -1,29 +1,40 @@
 <legend><?=$this->getTrans('receiver') ?></legend>
-<div class="table-responsive">
-    <table class="table table-hover table-striped">
-        <colgroup>
-            <col class="col-lg-2">
-            <col>
-        </colgroup>
-        <thead>
-            <tr>
-                <th><?=$this->getTrans('userName') ?></th>
-                <th><?=$this->getTrans('userEmail') ?></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php if ($this->get('emails') != ''): ?>
-                <?php foreach ($this->get('userList') as $user): ?>
+<form class="form-horizontal" method="POST" action="">
+    <?=$this->getTokenField() ?>
+    <?php if ($this->get('emails') != ''): ?>
+        <div class="table-responsive">
+            <table class="table table-hover table-striped">
+                <colgroup>
+                    <col class="icon_width">
+                    <col class="icon_width">
+                    <col class="col-lg-2">
+                    <col>
+                </colgroup>
+                <thead>
                     <tr>
-                        <td><?=$this->escape($user['name']) ?></td>
-                        <td><?=($user['email'] === '') ? 'NotRegistUser' : $this->escape($user['email']) ?></td>
+                        <th><?=$this->getCheckAllCheckbox('check_entries') ?></th>
+                        <th></th>
+                        <th><?=$this->getTrans('userName') ?></th>
+                        <th><?=$this->getTrans('userEmail') ?></th>
                     </tr>
-                <?php endforeach; ?>
-            <?php else: ?>
-                <tr>
-                    <td colspan="2"><?=$this->getTrans('noEmails') ?></td>
-                </tr>
-            <?php endif; ?>
-        </tbody>
-    </table>
-</div>
+                </thead>
+                <tbody>
+                    <?php foreach ($this->get('userList') as $user): ?>
+                        <tr>
+                            <td><input type="checkbox" name="check_entries[]" value="<?=$user['email'] ?>" /></td>
+                            <td>
+                                <?php $deleteArray = ['action' => 'delete', 'selector' => $user['selector']] ?>
+                                <?=$this->getDeleteIcon($deleteArray) ?>
+                            </td>
+                            <td><?=$this->escape($user['name']) ?></td>
+                            <td><?=($user['email'] === '') ? 'NotRegistUser' : $this->escape($user['email']) ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+        <?=$this->getListBar(['delete' => 'delete']) ?>
+    <?php else: ?>
+        <?=$this->getTrans('noEmails') ?>
+    <?php endif; ?>
+</form>
