6b9c2c054f18ff8d00e550b538fcfc975a419896
blackcoder87 <blackcoder@gmx.de>
2016-11-02 09:10:28 +0100

    Use the validation-class for the gallery-settings

--- application/modules/gallery/views/admin/settings/index.php
+++ application/modules/gallery/views/admin/settings/index.php
@@ -1,7 +1,17 @@
 <legend><?=$this->getTrans('settings') ?></legend>
+<?php if ($this->validation()->hasErrors()): ?>
+    <div class="alert alert-danger" role="alert">
+        <strong> <?=$this->getTrans('errorsOccured') ?>:</strong>
+        <ul>
+            <?php foreach ($this->validation()->getErrorMessages() as $error): ?>
+                <li><?= $error; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+<?php endif; ?>
 <form class="form-horizontal" method="POST" action="">
     <?=$this->getTokenField() ?>
-    <div class="form-group">
+    <div class="form-group <?=$this->validation()->hasError('picturesPerPage') ? 'has-error' : '' ?>">
         <label for="picturesPerPageInput" class="col-lg-2 control-label">
             <?=$this->getTrans('picturesPerPage') ?>:
         </label>
