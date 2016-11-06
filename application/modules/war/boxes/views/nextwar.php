0f23af7b5bba347f693b7f686cbfacef234625b0
kveldscholten <kevin_veldscholten@online.de>
2016-11-06 14:26:50 +0100

    Fix war boxes game img

--- application/modules/war/boxes/views/nextwar.php
+++ application/modules/war/boxes/views/nextwar.php
@@ -5,10 +5,10 @@
         $warMapper = $this->get('warMapper');
         $warTime = $war->getWarTime();    
         $gameImg = $this->getBoxUrl('static/img/'.$war->getWarGame().'.png');
-        if ($warMapper->url_check($gameImg)) {
+        if ($warMapper->url_check($gameImg) AND file_exists(APPLICATION_PATH.'/modules/war/static/img/'.$war->getWarGame().'.png')) {
             $gameImg = '<img src="'.$this->getBoxUrl('static/img/'.$war->getWarGame().'.png').'" title="'.$this->escape($war->getWarGame()).'" width="16" height="16">';
         } else {
-            $gameImg = '<i class="fa fa-question-circle text-muted"></i>';        
+            $gameImg = '<i class="fa fa-question-circle text-muted" title="'.$war->getWarGame().'"></i>';
         }
         ?>
         <div class="nextwar-box">
