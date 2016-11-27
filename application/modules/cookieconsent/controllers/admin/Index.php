b8e0a51303605210ce5c3c1628f93fec81228a39
blackcoder87 <blackcoder@gmx.de>
2016-11-27 11:04:42 +0100

    Use the validation-class for the cookieconsent-module
    Ticket 402

--- application/modules/cookieconsent/controllers/admin/Index.php
+++ application/modules/cookieconsent/controllers/admin/Index.php
@@ -6,6 +6,8 @@
 
 namespace Modules\Cookieconsent\Controllers\Admin;
 
+use Ilch\Validation;
+
 class Index extends \Ilch\Controller\Admin
 {
     public function init()
@@ -33,12 +35,31 @@ class Index extends \Ilch\Controller\Admin
                 ->add($this->getTranslator()->trans('settings'), ['action' => 'index']);
 
         if ($this->getRequest()->isPost()) {
-            $this->getConfig()->set('cookie_consent', $this->getRequest()->getPost('cookieConsent'));
-            $this->getConfig()->set('cookie_consent_style', $this->getRequest()->getPost('cookieConsentStyle'));
-            $this->getConfig()->set('cookie_consent_pos', $this->getRequest()->getPost('cookieConsentPos'));
-            $this->getConfig()->set('cookie_consent_message', $this->getRequest()->getPost('cookieConsentMessage'));
-            $this->getConfig()->set('cookie_consent_text', $this->getRequest()->getPost('cookieConsentText'));
-            $this->addMessage('saveSuccess');
+            Validation::setCustomFieldAliases([
+                'cookieConsent' => 'cookieConsentShow',
+            ]);
+
+            $validation = Validation::create($this->getRequest()->getPost(), [
+                cookieConsent => 'required|numeric|min:0|max:1',
+                cookieConsentStyle => 'required',
+                cookieConsentPos => 'required',
+                cookieConsentMessage => 'required',
+                cookieConsentText => 'required'
+            ]);
+
+            if ($validation->isValid()) {
+                $this->getConfig()->set('cookie_consent', $this->getRequest()->getPost('cookieConsent'));
+                $this->getConfig()->set('cookie_consent_style', $this->getRequest()->getPost('cookieConsentStyle'));
+                $this->getConfig()->set('cookie_consent_pos', $this->getRequest()->getPost('cookieConsentPos'));
+                $this->getConfig()->set('cookie_consent_message', $this->getRequest()->getPost('cookieConsentMessage'));
+                $this->getConfig()->set('cookie_consent_text', $this->getRequest()->getPost('cookieConsentText'));
+                $this->addMessage('saveSuccess');
+            }
+
+            $this->redirect()
+                ->withInput()
+                ->withErrors($validation->getErrorBag())
+                ->to(['action' => 'index']);
         }
 
         $this->getView()->set('cookieConsent', $this->getConfig()->get('cookie_consent'));
