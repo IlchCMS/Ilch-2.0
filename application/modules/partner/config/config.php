7d94c9d9473924a673572a6c7f84b5945ada3c0b
kveldscholten <kevin_veldscholten@online.de>
2017-02-01 17:27:24 +0100

    Update composer for fa v. 4.7 and change partner module icon

--- application/modules/partner/config/config.php
+++ application/modules/partner/config/config.php
@@ -11,7 +11,7 @@ class Config extends \Ilch\Config\Install
     public $config = [
         'key' => 'partner',
         'version' => '1.0',
-        'icon_small' => 'fa-users',
+        'icon_small' => 'fa-handshake-o',
         'author' => 'Veldscholten, Kevin',
         'link' => 'http://ilch.de',
         'languages' => [
