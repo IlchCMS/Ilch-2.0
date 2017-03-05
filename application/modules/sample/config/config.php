d29960a8dc5b738a39cb3fc5b5b75a4d7477f3ff
Thomas Stantin <th.stantin@gmx.de>
2017-03-05 20:27:39 +0100

    Module Sample add public $config

--- application/modules/sample/config/config.php
+++ application/modules/sample/config/config.php
@@ -8,7 +8,25 @@ namespace Modules\Sample\Config;
 
 class Config extends \Ilch\Config\Install
 {
-    //public $config = array ...
+    public $config = [
+        'key' => 'sample',
+        'version' => '1.0',
+        'icon_small' => '',
+        'author' => '',
+        'link' => '',
+        'languages' => [
+            'de_DE' => [
+                'name' => 'Sample',
+                'description' => '',
+            ],
+            'en_EN' => [
+                'name' => 'Sample',
+                'description' => '',
+            ],
+        ],
+        'ilchCore' => '2.0.0',
+        'phpVersion' => '5.6'
+    ];
 
     public function install()
     {
