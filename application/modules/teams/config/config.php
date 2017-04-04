806649e69391f7557e734449146ba211525c8a61
kveldscholten <kevin_veldscholten@online.de>
2017-04-04 15:50:09 +0200

    Update for english mails

--- application/modules/teams/config/config.php
+++ application/modules/teams/config/config.php
@@ -33,51 +33,51 @@ class Config extends \Ilch\Config\Install
         $this->db()->queryMulti($this->getInstallSql());
 
         $databaseConfig = new \Ilch\Config\Database($this->db());
-        $databaseConfig->set('teams_uploadpath', 'application/modules/teams/static/upload/');
-        $databaseConfig->set('teams_height', '80');
-        $databaseConfig->set('teams_width', '530');
-        $databaseConfig->set('teams_filetypes', 'jpg jpeg png');
+        $databaseConfig->set('teams_uploadpath', 'application/modules/teams/static/upload/')
+            ->set('teams_height', '80')
+            ->set('teams_width', '530')
+            ->set('teams_filetypes', 'jpg jpeg png');
     }
 
     public function uninstall()
     {
         $this->db()->queryMulti('DROP TABLE `[prefix]_teams`;
-                                 DROP TABLE `[prefix]_teams_joins`;');
+             DROP TABLE `[prefix]_teams_joins`;');
         $this->db()->queryMulti("DELETE FROM `[prefix]_config` WHERE `key` = 'teams_uploadpath';
-                                 DELETE FROM `[prefix]_config` WHERE `key` = 'teams_height';
-                                 DELETE FROM `[prefix]_config` WHERE `key` = 'teams_width';
-                                 DELETE FROM `[prefix]_config` WHERE `key` = 'teams_filetypes';
-                                 DELETE FROM `[prefix]_modules_folderrights` WHERE `key` = 'teams';
-                                 DELETE FROM `[prefix]_emails` WHERE `key` = 'teams'");
+             DELETE FROM `[prefix]_config` WHERE `key` = 'teams_height';
+             DELETE FROM `[prefix]_config` WHERE `key` = 'teams_width';
+             DELETE FROM `[prefix]_config` WHERE `key` = 'teams_filetypes';
+             DELETE FROM `[prefix]_modules_folderrights` WHERE `key` = 'teams';
+             DELETE FROM `[prefix]_emails` WHERE `key` = 'teams'");
     }
 
     public function getInstallSql()
     {
         return 'CREATE TABLE IF NOT EXISTS `[prefix]_teams` (
-              `id` INT(11) NOT NULL AUTO_INCREMENT,
-              `name` VARCHAR(255) NOT NULL,
-              `img` VARCHAR(255) NOT NULL,
-              `leader` VARCHAR(255) NOT NULL,
-              `coLeader` VARCHAR(255) NULL DEFAULT NULL,
-              `groupId` INT(11) NOT NULL,
-              `optIn` INT(1) NOT NULL,
-              PRIMARY KEY (`id`)
+                `id` INT(11) NOT NULL AUTO_INCREMENT,
+                `name` VARCHAR(255) NOT NULL,
+                `img` VARCHAR(255) NOT NULL,
+                `leader` VARCHAR(255) NOT NULL,
+                `coLeader` VARCHAR(255) NULL DEFAULT NULL,
+                `groupId` INT(11) NOT NULL,
+                `optIn` INT(1) NOT NULL,
+                PRIMARY KEY (`id`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
             CREATE TABLE IF NOT EXISTS `[prefix]_teams_joins` (
-              `id` INT(11) NOT NULL AUTO_INCREMENT,
-              `userId` INT(11) NULL DEFAULT NULL,
-              `name` VARCHAR(255) NOT NULL,
-              `email` VARCHAR(255) NOT NULL,
-              `place` VARCHAR(255) NOT NULL,
-              `birthday` DATE NOT NULL,
-              `gender` INT(1) NOT NULL,
-              `skill` INT(1) NOT NULL,
-              `teamId` INT(11) NOT NULL,
-              `locale` VARCHAR(255) NOT NULL,
-              `dateCreated` DATETIME NOT NULL,
-              `text` LONGTEXT NOT NULL,
-              PRIMARY KEY (`id`)
+                `id` INT(11) NOT NULL AUTO_INCREMENT,
+                `userId` INT(11) NULL DEFAULT NULL,
+                `name` VARCHAR(255) NOT NULL,
+                `email` VARCHAR(255) NOT NULL,
+                `place` VARCHAR(255) NOT NULL,
+                `birthday` DATE NOT NULL,
+                `gender` INT(1) NOT NULL,
+                `skill` INT(1) NOT NULL,
+                `teamId` INT(11) NOT NULL,
+                `locale` VARCHAR(255) NOT NULL,
+                `dateCreated` DATETIME NOT NULL,
+                `text` LONGTEXT NOT NULL,
+                PRIMARY KEY (`id`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
             INSERT INTO `[prefix]_modules_folderrights` (`key`, `folder`) VALUES
@@ -92,18 +92,38 @@ class Config extends \Ilch\Config\Install
                   <p>&nbsp;</p>
                   <p>Mit freundlichen Gr&uuml;&szlig;en</p>
                   <p>Administrator</p>", "de_DE"),
+            ("teams", "teams_accept_mail", "Accept application", "<p>Hello <b>{name}</b>,</p>
+                  <p>&nbsp;</p>
+                  <p>Your application at <i>{sitetitle}</i> in Team <i>{teamname}</i> was accepted.</p>
+                  <p>An account has been created with the acceptance, please click on the following link to assign a password</p>
+                  <p>{confirm}</p>
+                  <p>&nbsp;</p>
+                  <p>Best regards</p>
+                  <p>Administrator</p>", "en_EN"),
             ("teams", "teams_accept_user_mail", "User Bewerbung annehmen", "<p>Hallo <b>{name}</b>,</p>
                   <p>&nbsp;</p>
                   <p>deine Bewerbung auf <i>{sitetitle}</i> im Team <i>{teamname}</i> wurde angenommen.</p>
                   <p>&nbsp;</p>
                   <p>Mit freundlichen Gr&uuml;&szlig;en</p>
                   <p>Administrator</p>", "de_DE"),
+            ("teams", "teams_accept_user_mail", "Accept user application", "<p>Hello <b>{name}</b>,</p>
+                  <p>&nbsp;</p>
+                  <p>Your application at <i>{sitetitle}</i> in Team <i>{teamname}</i> was accepted.</p>
+                  <p>&nbsp;</p>
+                  <p>Best regards</p>
+                  <p>Administrator</p>", "en_EN"),
             ("teams", "teams_reject_mail", "Bewerbung ablehnen", "<p>Hallo <b>{name}</b>,</p>
                   <p>&nbsp;</p>
                   <p>deine Bewerbung auf <i>{sitetitle}</i> im Team <i>{teamname}</i> wurde leider abgelehnt.</p>
                   <p>&nbsp;</p>
                   <p>Mit freundlichen Gr&uuml;&szlig;en</p>
-                  <p>Administrator</p>", "de_DE");';
+                  <p>Administrator</p>", "de_DE"),
+            ("teams", "teams_reject_mail", "Reject application", "<p>Hello <b>{name}</b>,</p>
+                  <p>&nbsp;</p>
+                  <p>Your application at <i>{sitetitle}</i> in Team <i>{teamname}</i> was unfortunately rejected.</p>
+                  <p>&nbsp;</p>
+                  <p>Best regards</p>
+                  <p>Administrator</p>", "en_EN");';
     }
 
     public function getUpdate($installedVersion)
