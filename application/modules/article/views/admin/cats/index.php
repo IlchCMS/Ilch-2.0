d5d6c45f781eb7fdb97787091e9803ffe13ad82c
kveldscholten <kevin_veldscholten@online.de>
2017-04-06 00:18:56 +0200

    Update table sort

--- application/modules/article/views/admin/cats/index.php
+++ application/modules/article/views/admin/cats/index.php
@@ -4,14 +4,16 @@
     <div class="table-responsive">
         <table class="table table-hover table-striped">
             <colgroup>
-                <col class="icon_width">
-                <col class="icon_width">
-                <col class="icon_width">
-                <col>
+                <col class="icon_width" />
+                <col class="icon_width" />
+                <col class="icon_width" />
+                <col class="icon_width" />
+                <col />
             </colgroup>
             <thead>
                 <tr>
                     <th><?=$this->getCheckAllCheckbox('check_cats') ?></th>
+                    <th></th>
                     <th></th>
                     <th></th>
                     <th><?=$this->getTrans('title') ?></th>
@@ -25,12 +27,13 @@
                             <td><?=$this->getDeleteCheckbox('check_cats', $cat->getId()) ?></td>
                             <td><?=$this->getEditIcon(['action' => 'treat', 'id' => $cat->getId()]) ?></td>
                             <td><?=$this->getDeleteIcon(['action' => 'delcat', 'id' => $cat->getId()]) ?></td>
+                            <td><i class="fa fa-sort"></i></td>
                             <td><?=$this->escape($cat->getName()) ?></td>
                         </tr>
                     <?php endforeach; ?>
                 <?php else: ?>
                     <tr>
-                        <td colspan="4"><?=$this->getTrans('noCats') ?></td>
+                        <td colspan="5"><?=$this->getTrans('noCats') ?></td>
                     </tr>
                 <?php endif; ?>
             </tbody>
@@ -53,5 +56,14 @@
 </form>
 
 <script>
-$('.table tbody').sortable();
+    $('table tbody').sortable({
+        handle: '.fa-sort',
+        cursorAt: { left: 15 },
+        placeholder: "table-sort-drop",
+        forcePlaceholderSize: true,
+        'start': function (event, ui) {
+            ui.placeholder.html("<td colspan='5'></td>");
+            ui.placeholder.height(ui.item.height());
+        }
+    }).disableSelection();
 </script>
