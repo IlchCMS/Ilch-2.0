3c4476007083003361f81bcd707fcaa061f78917
blackcoder87 <blackcoder@gmx.de>
2017-04-13 09:08:01 +0200

    Fix usage of label

--- application/modules/admin/views/admin/menu/index.php
+++ application/modules/admin/views/admin/menu/index.php
@@ -136,7 +136,7 @@ function buildMenu($parentId, $menuData, View $view) {
             </div>
             <div class="dyn"></div>
             <div class="form-group">
-                <label for="assignedGroups" class="col-lg-4 control-label">
+                <label for="access" class="col-lg-4 control-label">
                     <?=$this->getTrans('notVisible') ?>
                 </label>
                 <div class="col-lg-8">
@@ -351,7 +351,7 @@ $(document).ready
                 return;
             }
 
-            menuHtml = '<div class="form-group"><label for="href" class="col-lg-4 control-label"><?=$this->getTrans('labelMenu') ?></label>\n\
+            menuHtml = '<div class="form-group"><label for="menukey" class="col-lg-4 control-label"><?=$this->getTrans('labelMenu') ?></label>\n\
                         <div class="col-lg-8"><select class="form-control" id="menukey">'+options+'</select></div></div>';
 
             if ($(this).val() == '0') {
@@ -360,13 +360,13 @@ $(document).ready
                 $('.dyn').html('<div class="form-group"><label for="href" class="col-lg-4 control-label"><?=$this->getTrans('address') ?></label>\n\
                                 <div class="col-lg-8"><input type="text" class="form-control" id="href" value="http://" /></div></div>'+menuHtml);
             } else if ($(this).val() == '2') {
-                 $('.dyn').html('<div class="form-group"><label for="href" class="col-lg-4 control-label"><?=$this->getTrans('page') ?></label>\n\
+                 $('.dyn').html('<div class="form-group"><label for="siteid" class="col-lg-4 control-label"><?=$this->getTrans('page') ?></label>\n\
                                 <div class="col-lg-8"><?php if (!empty($pages)) { echo '<select class="form-control" id="siteid">'; foreach ($pages as $page) { echo '<option value="'.$page->getId().'">'.$this->escape($page->getTitle()).'</option>';} echo '</select>'; } else { echo $this->getTrans('missingSite'); } ?></div></div>'+menuHtml);
             } else if ($(this).val() == '3') {
-                $('.dyn').html('<div class="form-group"><label for="href" class="col-lg-4 control-label"><?=$this->getTrans('module') ?></label>\n\
+                $('.dyn').html('<div class="form-group"><label for="modulekey" class="col-lg-4 control-label"><?=$this->getTrans('module') ?></label>\n\
                                 <div class="col-lg-8"><?php if (!empty($modules)) { echo '<select class="form-control" id="modulekey">'; foreach ($modules as $module) { $content = $module->getContentForLocale($this->getTranslator()->getLocale()); echo '<option value="'.$module->getKey().'">'.$content['name'].'</option>';} echo '</select>'; } else { echo $this->getTrans('missingModule'); } ?></div></div>'+menuHtml);
             } else if ($(this).val() == '4') {
-                $('.dyn').html('<div class="form-group"><label for="href" class="col-lg-4 control-label"><?=$this->getTrans('box') ?></label>\n\
+                $('.dyn').html('<div class="form-group"><label for="boxkey" class="col-lg-4 control-label"><?=$this->getTrans('box') ?></label>\n\
                                 <div class="col-lg-8"><?='<select class="form-control" id="boxkey">';
                 foreach ($boxes as $box) { echo '<option value="'.$box->getModule().'_'.$box->getKey().'">'.$box->getName().'</option>'; } foreach ($selfBoxes as $box) { echo '<option value="'.$box->getId().'">self_'.$this->escape($box->getTitle()).'</option>';} echo '</select>'; ?></div></div>');
             }
