30cbcddcb0de504baa7848249f4479699e637fdf
blackcoder87 <blackcoder@gmx.de>
2017-08-01 23:56:47 +0200

    Update config.php of clanlayout-module

--- application/modules/clanlayout/config/config.php
+++ application/modules/clanlayout/config/config.php
@@ -67,27 +67,6 @@ class Config extends \Ilch\Config\Install
 
     public function getUpdate($installedVersion)
     {
-        switch ($installedVersion) {
-            case "1.0":
-                $menuMapper = new \Modules\Admin\Mappers\Menu();
-                $lastMenuId = $menuMapper->getLastMenuId()+1;
-                $lastMenuItemId = $menuMapper->getLastMenuItemId()+1;
 
-                return 'CREATE TABLE IF NOT EXISTS `[prefix]_clanlayout_settings` (
-                        `key` VARCHAR(255) NOT NULL,
-                        `value` TEXT NOT NULL,
-                        UNIQUE KEY `key` (`key`)
-                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-        
-                    INSERT INTO `[prefix]_clanlayout_settings` (`key`, `value`) VALUES ("headername", "Clanname");
-        
-                    INSERT INTO `[prefix]_menu` (`id`, `title`) VALUES ('.$lastMenuId.', "Footermenü");
-                    
-                    INSERT INTO `[prefix]_menu_items` (`menu_id`, `sort`, `parent_id`, `page_id`, `box_id`, `type`, `title`, `module_key`) VALUES
-                        ('.$lastMenuId.', 0, 0, 0, 0, 3, "Menü", ""),
-                        ('.$lastMenuId.', 10, '.$lastMenuItemId.', 0, 0, 3, "Kontakt", "contact"),
-                        ('.$lastMenuId.', 20, '.$lastMenuItemId.', 0, 0, 3, "Impressum", "imprint"),
-                        ('.$lastMenuId.', 30, '.$lastMenuItemId.', 0, 0, 3, "Datenschutz", "privacy")';
-        }
     }
 }
