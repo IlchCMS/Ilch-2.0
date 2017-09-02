ce18a2a1d7d03668017b0c8a9606f970281e2e45
blackcoder87 <blackcoder@gmx.de>
2017-09-02 11:50:27 +0200

    Some changes to send mail on user creation

--- application/modules/user/views/admin/index/treat.php
+++ application/modules/user/views/admin/index/treat.php
@@ -46,7 +46,7 @@ if ($user->getId()) {
         </label>
         <div class="col-lg-9">
             <input type="password"
-                   class="form-control required"
+                   class="form-control"
                    id="userPassword"
                    name="user[password]"
                    placeholder="<?=$this->getTrans('userPassword') ?>"
