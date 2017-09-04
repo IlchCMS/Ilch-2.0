caa61f887b2f46e6140dc0b0e547d4bd87acac1f
blackcoder87 <blackcoder@gmx.de>
2017-09-04 09:23:00 +0200

    Consider readAccess when displaying events in calendar

--- application/modules/events/controllers/Events.php
+++ application/modules/events/controllers/Events.php
@@ -7,15 +7,32 @@
 namespace Modules\Events\Controllers;
 
 use Modules\Events\Mappers\Events as EventsMapper;
+use Modules\User\Mappers\User as UserMapper;
 
 class Events extends \Ilch\Controller\Frontend
 {
     public function indexAction()
     {
         $eventsMapper = new EventsMapper();
+        $userMapper = new UserMapper();
 
         $this->getLayout()->setFile('modules/calendar/layouts/events');
 
-        $this->getView()->set('eventList', $eventsMapper->getEntriesForJson($this->getRequest()->getQuery('start'), $this->getRequest()->getQuery('end')));
+        $userId = null;
+        if ($this->getUser()) {
+            $userId = $this->getUser()->getId();
+        }
+        $user = $userMapper->getUserById($userId);
+        $ids = [3];
+        if ($user) {
+            $ids = [];
+            foreach ($user->getGroups() as $us) {
+                $ids[] = $us->getId();
+            }
+        }
+        $readAccess = explode(',',implode(',', $ids));
+
+        $this->getView()->set('eventList', $eventsMapper->getEntriesForJson($this->getRequest()->getQuery('start'), $this->getRequest()->getQuery('end')))
+            ->set('readAccess', $readAccess);
     }
 }
--- application/modules/events/views/events/index.php
+++ application/modules/events/views/events/index.php
@@ -1,9 +1,17 @@
 <?php
 $events = [];
 
+$adminAccess = null;
+if ($this->getUser()) {
+    $adminAccess = $this->getUser()->isAdmin();
+}
+
 // event entries
 if ($this->get('eventList')) {
     foreach ($this->get('eventList') as $eventList) {
+        if (!is_in_array($this->get('readAccess'), explode(',', $eventList->getReadAccess())) && $adminAccess == false) {
+            continue;
+        }
         $e = [];
         $e['title'] = $this->escape($eventList->getTitle());
         $e['start'] = $eventList->getStart();
