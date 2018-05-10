ac14343784b4340fb8536234299fa84f513ee418
blackcoder87 <blackcoder@gmx.de>
2018-05-10 13:40:41 +0200

    Update config.php

--- application/modules/vote/config/config.php
+++ application/modules/vote/config/config.php
@@ -10,7 +10,7 @@ class Config extends \Ilch\Config\Install
 {
     public $config = [
         'key' => 'vote',
-        'version' => '1.3',
+        'version' => '1.4',
         'icon_small' => 'fa-tasks',
         'author' => 'Veldscholten, Kevin',
         'link' => 'http://ilch.de',
