acc5a43d3db26fbd36f27753b8247c7f78e27aee
blackcoder87 <blackcoder@gmx.de>
2018-06-04 07:23:03 +0200

    Ticket 525: Error 404 Controller "index"
    Add index controller and redirect to privacy modue.
    http://redmine.ilch2.de/issues/525

---
+++ application/modules/cookieconsent/controllers/Index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright Ilch 2.0
+ * @package ilch
+ */
+
+namespace Modules\Cookieconsent\Controllers;
+
+class Index extends \Ilch\Controller\FrontEnd
+{
+    public function init()
+    {
+    }
+
+    public function indexAction()
+    {
+        $this->redirect(['module' => 'privacy', 'controller' => 'index', 'action' => 'index']);
+    }
+}
