7ccd1ffcc5ba85df822362c7c2e4132901cf996d
blackcoder87 <blackcoder@gmx.de>
2018-06-07 07:39:43 +0200

    Update version numbers

--- application/modules/gallery/config/config.php
+++ application/modules/gallery/config/config.php
@@ -10,7 +10,7 @@ class Config extends \Ilch\Config\Install
 {
     public $config = [
         'key' => 'gallery',
-        'version' => '1.2',
+        'version' => '1.3',
         'icon_small' => 'fa-picture-o',
         'author' => 'Stantin, Thomas',
         'link' => 'http://ilch.de',
