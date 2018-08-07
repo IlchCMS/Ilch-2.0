48ea4c8f9c76fee07d3dfc6cec5fc857552becb5
blackcoder87 <blackcoder@gmx.de>
2018-08-07 14:45:57 +0200

    Fix wrong variable name

--- application/modules/admin/views/admin/modules/search.php
+++ application/modules/admin/views/admin/modules/search.php
@@ -141,7 +141,7 @@ if (empty($modulesOnUpdateServer)) {
                                 <i class="<?=$iconClass ?>"></i>
                             </button>
                         <?php elseif ($isInstalled && version_compare($versionsOfModules[$moduleOnUpdateServer->key]['version'], $moduleOnUpdateServer->version, '<')): ?>
-                            <form method="POST" action="<?=$this->getUrl(['action' => 'update', 'key' => $moduleOnUpdateServer->key, 'version' => $moduleOnUpdateServerFound->version, 'from' => 'search']) ?>">
+                            <form method="POST" action="<?=$this->getUrl(['action' => 'update', 'key' => $moduleOnUpdateServer->key, 'version' => $moduleOnUpdateServer->version, 'from' => 'search']) ?>">
                                 <?=$this->getTokenField() ?>
                                 <button type="submit"
                                         class="btn btn-default"
@@ -150,7 +150,7 @@ if (empty($modulesOnUpdateServer)) {
                                 </button>
                             </form>
                         <?php else: ?>
-                            <form method="POST" action="<?=$this->getUrl(['action' => 'search', 'key' => $moduleOnUpdateServer->key, 'version' => $moduleOnUpdateServerFound->version]) ?>">
+                            <form method="POST" action="<?=$this->getUrl(['action' => 'search', 'key' => $moduleOnUpdateServer->key, 'version' => $moduleOnUpdateServer->version]) ?>">
                                 <?=$this->getTokenField() ?>
                                 <button type="submit"
                                         class="btn btn-default"
