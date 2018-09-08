9ff4bd8d042d977b76f2d8c1f73a275b0547c9b1
kveldscholten <kevin_veldscholten@online.de>
2018-09-08 15:27:47 +0200

    Test max varchar to 191

--- tests/PHPUnit/Ilch/_files/db_schema.sql
+++ tests/PHPUnit/Ilch/_files/db_schema.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `config` (
-  `key` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `key` varchar(191) COLLATE utf8mb4_general_ci NOT NULL,
   `value` text COLLATE utf8mb4_general_ci NOT NULL,
   `autoload` tinyint(1) NOT NULL,
   UNIQUE KEY `key` (`key`)
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `config` (
 
 CREATE TABLE IF NOT EXISTS `groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=7 ;
 
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `groups` (
 
 CREATE TABLE IF NOT EXISTS `menu` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
+  `title` varchar(191) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=3 ;
 
@@ -59,8 +59,8 @@ CREATE TABLE IF NOT EXISTS `menu_items` (
   `parent_id` int(11) NOT NULL,
   `page_id` int(11) NOT NULL,
   `type` int(11) NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `href` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `title` varchar(191) COLLATE utf8mb4_general_ci NOT NULL,
+  `href` varchar(191) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci AUTO_INCREMENT=3 ;
 
@@ -72,8 +72,8 @@ CREATE TABLE IF NOT EXISTS `menu_items` (
 
 CREATE TABLE IF NOT EXISTS `modules` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `key` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `icon_small` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `key` varchar(191) COLLATE utf8mb4_general_ci NOT NULL,
+  `icon_small` varchar(191) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `key` (`key`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci AUTO_INCREMENT=3 ;
@@ -99,9 +99,9 @@ CREATE TABLE IF NOT EXISTS `pages` (
 CREATE TABLE IF NOT EXISTS `pages_content` (
   `page_id` int(11) NOT NULL,
   `content` mediumtext COLLATE utf8mb4_general_ci NOT NULL,
-  `locale` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `perma` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
+  `locale` varchar(191) COLLATE utf8mb4_general_ci NOT NULL,
+  `title` varchar(191) COLLATE utf8mb4_general_ci NOT NULL,
+  `perma` varchar(191) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -112,9 +112,9 @@ CREATE TABLE IF NOT EXISTS `pages_content` (
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_general_ci NOT NULL,
   `date_created` datetime NOT NULL,
   `date_confirmed` datetime NOT NULL,
   PRIMARY KEY (`id`)
