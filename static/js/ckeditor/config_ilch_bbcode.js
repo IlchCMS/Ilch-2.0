e047f7ac9bd0335501798491bb6e19551ae2314a
blackcoder87 <blackcoder@gmx.de>
2018-09-08 06:59:57 +0200

    Enable smiley plugin to avoid error
    Enable smiley plugin to avoid "Cannot read property 'indexOf' of undefined.
    
    This error made it for example impossible to edit a post in the forum, which contained ";)".

--- static/js/ckeditor/config_ilch_bbcode.js
+++ static/js/ckeditor/config_ilch_bbcode.js
@@ -7,7 +7,7 @@ CKEDITOR.editorConfig = function (config) {
     if (typeof ilchSmileysPlugin !== "undefined") {
         CKEDITOR.plugins.addExternal('ilchsmileys', ilchSmileysPlugin);
         CKEDITOR.plugins.addExternal('ilchyoutube', ilchYoutubePlugin);
-        config.extraPlugins = "bbcode,font,colorbutton,codesnippet,ilchsmileys,ilchyoutube";
+        config.extraPlugins = "bbcode,font,colorbutton,codesnippet,smiley,ilchsmileys,ilchyoutube";
         config.fontSize_sizes = "30/30%;50/50%;100/100%;120/120%;150/150%;200/200%;300/300%";
     }
     config.toolbar = 'ilch_bbcode';
