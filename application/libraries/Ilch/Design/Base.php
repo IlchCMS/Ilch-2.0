5c6aa276a07b48651857e021dfbe4d024cbdbfeb
blackcoder87 <blackcoder@gmx.de>
2018-09-20 17:46:54 +0200

    Specify charset and disable double_encode
    double_encode caused problems with the code tag in ckeditor.

--- application/libraries/Ilch/Design/Base.php
+++ application/libraries/Ilch/Design/Base.php
@@ -303,7 +303,7 @@ abstract class Base
      */
     public function escape($string)
     {
-        return htmlspecialchars($string, ENT_QUOTES);
+        return htmlspecialchars($string, ENT_QUOTES, 'UTF-8', false);
     }
 
     /**
