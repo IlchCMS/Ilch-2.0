98f5e91f9c9af39e0c49c6bbdcb118909b91f8bf
blackcoder87 <blackcoder@gmx.de>
2018-09-22 11:50:31 +0200

    Update ilchCore requirements

--- application/modules/faq/config/config.php
+++ application/modules/faq/config/config.php
@@ -24,7 +24,7 @@ class Config extends \Ilch\Config\Install
                 'description' => 'Here you can manage your FAQ - Frequently Asked Questions.',
             ],
         ],
-        'ilchCore' => '2.0.0',
+        'ilchCore' => '2.1.15',
         'phpVersion' => '5.6'
     ];
 
--- application/modules/history/config/config.php
+++ application/modules/history/config/config.php
@@ -24,7 +24,7 @@ class Config extends \Ilch\Config\Install
                 'description' => 'Here you can create a history for your site.',
             ],
         ],
-        'ilchCore' => '2.0.0',
+        'ilchCore' => '2.1.15',
         'phpVersion' => '5.6'
     ];
 
