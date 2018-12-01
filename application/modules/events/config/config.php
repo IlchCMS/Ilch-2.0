2f4a51e30a8840797ee3b734d823195efa3f8301
blackcoder87 <blackcoder@gmx.de>
2018-12-01 09:15:05 +0100

    Update ilchCore requirement

--- application/modules/events/config/config.php
+++ application/modules/events/config/config.php
@@ -36,7 +36,7 @@ class Config extends \Ilch\Config\Install
                 ]
             ]
         ],
-        'ilchCore' => '2.1.16',
+        'ilchCore' => '2.1.18',
         'phpVersion' => '5.6'
     ];
 
