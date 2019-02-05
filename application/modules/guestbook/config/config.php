5bf4d4a541f6896c109cd5fcee2e2fb59987be62
blackcoder87 <blackcoder@gmx.de>
2019-02-05 17:17:46 +0100

    cosmetic fixes

--- application/modules/guestbook/config/config.php
+++ application/modules/guestbook/config/config.php
@@ -10,7 +10,7 @@ class Config extends \Ilch\Config\Install
 {
     public $config = [
         'key' => 'guestbook',
-        'version' => '1.3.0',
+        'version' => '1.4.0',
         'icon_small' => 'fa-book',
         'author' => 'Stantin, Thomas',
         'link' => 'http://ilch.de',
