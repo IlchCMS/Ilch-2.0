8c36e0564a8b9b55c77ac0c4e690339b8b245cfd
blackcoder87 <blackcoder@gmx.de>
2019-06-06 23:01:53 +0200

    Remove sorting in just category management
    This collided with the general sorting of all items (categories and rules).

--- application/modules/rule/views/admin/cats/index.php
+++ application/modules/rule/views/admin/cats/index.php
@@ -7,13 +7,11 @@
                 <col class="icon_width" />
                 <col class="icon_width" />
                 <col class="icon_width" />
-                <col class="icon_width" />
                 <col />
             </colgroup>
             <thead>
                 <tr>
                     <th><?=$this->getCheckAllCheckbox('check_cats') ?></th>
-                    <th></th>
                     <th></th>
                     <th></th>
                     <th><?=$this->getTrans('title') ?></th>
@@ -27,7 +25,6 @@
                             <td><?=$this->getDeleteCheckbox('check_cats', $cat->getId()) ?></td>
                             <td><?=$this->getEditIcon(['action' => 'treat', 'id' => $cat->getId()]) ?></td>
                             <td><?=$this->getDeleteIcon(['action' => 'delcat', 'id' => $cat->getId()]) ?></td>
-                            <td><i class="fa fa-sort"></i></td>
                             <td><?=$this->escape($cat->getTitle()) ?></td>
                         </tr>
                     <?php endforeach; ?>
@@ -40,7 +37,6 @@
         </table>
     </div>
     <div class="content_savebox">
-        <input type="hidden" class="content_savebox_hidden" name="action" value="" />
         <div class="btn-group dropup">
             <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
                 <?=$this->getTrans('selected') ?> <span class="caret"></span>
@@ -49,21 +45,5 @@
                 <li><a href="#" data-hiddenkey="delete"><?=$this->getTrans('delete') ?></a></li>
             </ul>
         </div>
-        <button type="submit" class="save_button btn btn-default" name="saveCats" value="save">
-            <?=$this->getTrans('saveButton') ?>
-        </button>
     </div>
 </form>
-
-<script>
-$('table tbody').sortable({
-    handle: 'td',
-    cursorAt: { left: 15 },
-    placeholder: "table-sort-drop",
-    forcePlaceholderSize: true,
-    'start': function (event, ui) {
-        ui.placeholder.html("<td colspan='5'></td>");
-        ui.placeholder.height(ui.item.height());
-    }
-}).disableSelection();
-</script>
