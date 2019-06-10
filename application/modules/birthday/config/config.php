17efca709f842b0942d5b8736e702c6ec3d46491
blackcoder87 <blackcoder@gmx.de>
2019-06-10 11:53:50 +0200

    Update version number of birthday module

--- application/modules/birthday/config/config.php
+++ application/modules/birthday/config/config.php
@@ -10,7 +10,7 @@ class Config extends \Ilch\Config\Install
 {
     public $config = [
         'key' => 'birthday',
-        'version' => '1.2.0',
+        'version' => '1.3.0',
         'icon_small' => 'fa-birthday-cake',
         'author' => 'Veldscholten, Kevin',
         'link' => 'http://ilch.de',
