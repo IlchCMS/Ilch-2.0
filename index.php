37d8d8c924b4e54ee69cc18d22668440b5c10b87
blackcoder87 <blackcoder@gmx.de>
2020-03-27 11:53:19 +0100

    Update version number and some small fixes

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @copyright Ilch 2.0
+ * @copyright Ilch 2
  * @package ilch
  */
 
 if (!version_compare(phpversion(), '5.6.0', '>=')) {
-    die('Ilch CMS 2.* needed minimum php version 5.6.0');
+    die('Ilch CMS 2 needs at least php version 5.6.0');
 }
 
 @ini_set('display_errors', 'on');
@@ -29,16 +29,14 @@ header('Content-Type: text/html; charset=utf-8');
 $serverTimeZone = @date_default_timezone_get();
 date_default_timezone_set('UTC');
 
-define('VERSION', '2.1.31');
+define('VERSION', '2.1.32');
 define('ILCH_SERVER', 'http://www.ilch.de/ilch2');
 define('SERVER_TIMEZONE', $serverTimeZone);
 define('DEFAULT_MODULE', 'page');
 define('DEFAULT_LAYOUT', 'index');
 define('DEBUG_MODE', true);
 
-/*
- * Path could not be under root.
- */
+// Path could not be under root.
 define('ROOT_PATH', __DIR__);
 define('APPLICATION_PATH', __DIR__.'/application');
 define('CONFIG_PATH', APPLICATION_PATH);
@@ -50,7 +48,7 @@ define('REWRITE_BASE', $rewriteBaseParts);
 $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
 define('BASE_URL', $protocol.'://'.$_SERVER['HTTP_HOST'].REWRITE_BASE);
 
-//register autoloaders
+// register autoloaders
 require ROOT_PATH . '/vendor/autoload.php';
 $loader = new \Ilch\Loader();
 
