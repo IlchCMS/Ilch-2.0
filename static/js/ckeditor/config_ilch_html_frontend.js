c50c7605e6c3415b32bd5d440fe4c3069c84da02
blackcoder87 <blackcoder@gmx.de>
2020-09-12 09:18:24 +0200

    Simplify ckeditor config and adjust quote feature

--- static/js/ckeditor/config_ilch_html_frontend.js
+++ static/js/ckeditor/config_ilch_html_frontend.js
@@ -7,32 +7,24 @@ var basePath = CKEDITOR.basePath;
 basePath = basePath.substr(0, basePath.indexOf("vendor/ckeditor/"));
 
 CKEDITOR.plugins.addExternal('emojione', basePath+'static/js/ckeditor/plugins/emojione/');
-CKEDITOR.plugins.addExternal('ilchmedia', basePath+'application/modules/media/static/js/ilchmedia/');
 
 CKEDITOR.editorConfig = function( config ) {
-    if (typeof ilchPsPlugin !== "undefined") {
-        CKEDITOR.plugins.addExternal('ilchps', ilchPsPlugin);
-        config.extraPlugins = "justify,font,colorbutton,colordialog,tableresize,emojione,ilchmedia,ilchps";
-    } else {
-        config.extraPlugins = "justify,font,colorbutton,colordialog,tableresize,emojione,ilchmedia";
-    }
-
+    config.extraPlugins = "justify,font,colorbutton,colordialog,codesnippet,tableresize,emojione";
     config.protectedSource.push(/<i[^>]*><\/i>/g);
     config.protectedSource.push(/<\?[\s\S]*?\?>/g);
-    config.toolbar = 'ilch_html_frontend';
-    config.toolbar_ilch_html_frontend = [
-        { name: 'document', groups: [ 'mode' ], items: [ 'Source' ] },
-        { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
-        { name: 'editing', groups: [ 'spellchecker' ], items: [ 'Scayt' ] },
+    config.toolbarGroups = [
+        { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+        { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+        { name: 'links' },
+        { name: 'insert' },
+        { name: 'forms' },
+        { name: 'tools' },
+        { name: 'document',       groups: [ 'mode', 'document', 'doctools' ] },
+        { name: 'others' },
         '/',
-        { name: 'basicstyles', groups: [ 'basicstyles', 'alignment', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'RemoveFormat' ] },
-        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
-        { name: 'links', items: [ 'Link', 'Unlink', 'Emojione', 'Anchor' ] },
-        { name: 'insert', items: [ 'Table', 'HorizontalRule' ] },
-        '/',
-        { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
-        { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
-        { name: 'tools', items: [ 'Maximize' ] },
-        { name: 'insert', items: [ 'ilchmedia', 'ilchps' ] }
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+        { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+        { name: 'styles' },
+        { name: 'colors' }
     ];
 };
