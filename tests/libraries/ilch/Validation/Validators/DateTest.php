f251f8dc5734c8a93189eba062fc43687c42bc93
Dennis Reilard <hdallo@web.de>
2020-09-21 20:01:21 +0200

    Language Fix and TestValidator Update

--- tests/libraries/ilch/Validation/Validators/DateTest.php
+++ tests/libraries/ilch/Validation/Validators/DateTest.php
@@ -35,11 +35,40 @@ class DateTest extends TestCase
     public function dpForTestValidator()
     {
         return [
-            // string validations
+            // date validations
             'date correct' => [
                 'data'                    => $this->createData('2020-08-01'),
                 'expectedIsValid'         => true
             ],
+            'date wrong' => [
+                'data'                    => $this->createData('2020-08-01 12:00:00'),
+                'expectedIsValid'         => false,
+                'expectedErrorKey'        => 'validation.errors.date.mustBeDate',
+                'expectedErrorParameters' => ['Y-m-d']
+            ],
+            // datetime validations
+            'datetime correct' => [
+                'data'                    => $this->createData('2020-08-01 12:00:00', 'Y-m-d H\\:i\\:s'),
+                'expectedIsValid'         => true
+            ],
+            'datetime wrong' => [
+                'data'                    => $this->createData('2020-08-01 24:00:00', 'Y-m-d H\\:i\\:s'),
+                'expectedIsValid'         => false,
+                'expectedErrorKey'        => 'validation.errors.date.mustBeDate',
+                'expectedErrorParameters' => ['Y-m-d H\\:i\\:s']
+            ],
+            // time validations
+            'time correct' => [
+                'data'                    => $this->createData('12:00:00', 'H\\:i\\:s'),
+                'expectedIsValid'         => true
+            ],
+            'time wrong' => [
+                'data'                    => $this->createData('24:00:00', 'H\\:i\\:s'),
+                'expectedIsValid'         => false,
+                'expectedErrorKey'        => 'validation.errors.date.mustBeDate',
+                'expectedErrorParameters' => ['H\\:i\\:s']
+            ],
+            // other
             'string too short' => [
                 'data'                    => $this->createData('abcd'),
                 'expectedIsValid'         => false,
@@ -52,15 +81,14 @@ class DateTest extends TestCase
     /**
      * Helper function for creating data object
      * @param string $value
-     * @param int $min
-     * @param bool $forceString
+     * @param string $parameter
      * @return \stdClass
      */
-    private function createData($value)
+    private function createData($value, $parameter = 'Y-m-d')
     {
         $data = new \stdClass();
         $data->field = 'fieldName';
-        $data->parameters = [];
+        $data->parameters = [$parameter];
         $data->input = ['fieldName' => $value];
         return $data;
     }
