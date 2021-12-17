212362e218bc7563babf89a1b904023926ce06a7
Dennis Reilard <hdallo@web.de>
2021-12-17 14:14:15 +0100

    Versionsnummer update

--- application/modules/partner/config/config.php
+++ application/modules/partner/config/config.php
@@ -10,7 +10,7 @@ class Config extends \Ilch\Config\Install
 {
     public $config = [
         'key' => 'partner',
-        'version' => '1.10.0',
+        'version' => '1.11.0',
         'icon_small' => 'fa-handshake-o',
         'author' => 'Veldscholten, Kevin',
         'link' => 'https://ilch.de',
