3f5ae309260f25e667a0a179dbb7602215d18d1a
Dennis Reilard <hdallo@web.de>
2022-08-16 09:32:19 +0200

    Update Box.php

--- application/modules/admin/models/Box.php
+++ application/modules/admin/models/Box.php
@@ -288,9 +288,9 @@ class Box extends \Ilch\Model
      * Add content for given language.
      *
      * @param string $langKey
-     * @param string $content
+     * @param string|array $content
      */
-    public function addContent(string $langKey, string $content)
+    public function addContent(string $langKey, $content)
     {
         $this->content[$langKey] = $content;
     }
