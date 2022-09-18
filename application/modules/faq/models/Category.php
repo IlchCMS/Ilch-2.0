bb94c0ec2cd1a7ac94df1c04c931d90f3aa8707c
RTX2070 <barth.duvenstedt@gmx.de>
2022-09-18 12:58:49 +0200

    Update Category.php

--- application/modules/faq/models/Category.php
+++ application/modules/faq/models/Category.php
@@ -8,86 +8,42 @@ namespace Modules\Faq\Models;
 
 class Category extends \Ilch\Mapper
 {
-    /**
-     * The id of the category.
-     *
-     * @var int
-     */
+
     private $id;
-
-    /**
-     * The title of the category.
-     *
-     * @var string
-     */
     private $title;
-
-    /**
-     * Value for read_access.
-     *
-     * @var string
-     */
     private $read_access;
 
-    /**
-     * Gets the category id.
-     *
-     * @return int
-     */
+
+    public function __construct(array $data = [])
+    {
+        foreach ($data as $key => $value) {
+            $this->__set($key, $value);
+        }
+    }
+
+    public function __set($property, $value)
+    {
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        }
+
+        return $this;
+    }
+
+
     public function getId()
     {
         return $this->id;
     }
 
-    /**
-     * Sets the id of the category.
-     *
-     * @param int $id
-     */
-    public function setId($id)
-    {
-        $this->id = (int)$id;
-    }
-
-    /**
-     * Gets the title of the category.
-     *
-     * @return string
-     */
     public function getTitle()
     {
         return $this->title;
     }
 
-    /**
-     * Sets the title of the category.
-     *
-     * @param string $title
-     */
-    public function setTitle($title)
-    {
-        $this->title = (string)$title;
-    }
-
-    /**
-     * Get the value for read_access.
-     *
-     * @return string
-     */
     public function getReadAccess()
     {
         return $this->read_access;
     }
 
-    /**
-     * Set the value for read_access.
-     *
-     * @param string $read_access
-     * @return $this
-     */
-    public function setReadAccess($read_access)
-    {
-        $this->read_access = $read_access;
-        return $this;
-    }
 }
