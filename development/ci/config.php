400c49efb1a2f86569e42a3e8e43efea38337eb0
Sebastian Große <mairu@ilch.de>
2022-11-19 17:18:04 +0100

    Add GitHub actions workflow to run tests (#584)
    * Add GitHub actions workflow to run tests

---
+++ development/ci/config.php
@@ -0,0 +1,11 @@
+<?php
+//Config for Tests
+$config = [
+    "dbEngine"   => "Mysql",
+    "dbHost"     => "$HOST:$PORT",
+    "dbUser"     => "ilch2_test_user",
+    "dbPassword" => "ilch2_test_password",
+    "dbName"     => "ilch2_test",
+    "dbPrefix"   => "",
+];
+
