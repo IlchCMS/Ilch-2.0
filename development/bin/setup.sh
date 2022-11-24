6d50de4e1096f9d7052d7ac9aea8652108a07fc1
Dennis Reilard <hdallo@web.de>
2022-11-24 19:11:36 +0100

    Update setup.sh

--- development/bin/setup.sh
+++ development/bin/setup.sh
@@ -2,11 +2,13 @@
 DIR=`dirname $0`
 START_PWD=${PWD}
 cd ${DIR}
+echo "--- Install/Update Composer"
 if [ ! -f "composer.phar" ]; then
     curl -sS https://getcomposer.org/installer | php
     chmod +x composer.phar
 else
-    ./composer.phar self-update
+    php ./composer.phar self-update
+	chmod +x composer.phar
 fi
 echo "--- Update Development Vendor"
 cd ..
