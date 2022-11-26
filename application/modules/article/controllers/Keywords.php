fba10d542c5b6f7a400766c82ae2577f075ea181
blackcoder87 <blackcoder@gmx.de>
2022-11-26 10:01:05 +0100

    Fix "Uncaught TypeError" with invalid keyword
    Small drive by fix

--- application/modules/article/controllers/Keywords.php
+++ application/modules/article/controllers/Keywords.php
@@ -76,7 +76,7 @@ class Keywords extends \Ilch\Controller\Frontend
 
         $keyword = $this->getRequest()->getParam('keyword');
 
-        if (!empty($keyword) && !$articleMapper->keywordExists($keyword)) {
+        if (empty($keyword) && !$articleMapper->keywordExists($keyword)) {
             $this->addMessage('unknownKeyword', 'danger');
             $this->redirect()
                 ->to(['action' => 'index']);
