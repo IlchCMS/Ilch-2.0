8f9c92a77c87e03c018870c46573d83079ce19f1
blackcoder87 <blackcoder@gmx.de>
2022-12-13 14:34:26 +0100

    Update optimize_vendor, use minified venobox files (#604)
    * Update optimize_vendor, use minified venobox files
    
    - Update version number of the gallery and forum module
    - Use minified versions of the venobox files
    - Update optimize_vendor to only keep the files needed for venobox
    
    https://github.com/IlchCMS/Ilch-2.0/pull/603
    
    * Fix problem with new jquery.mjs.nestedSortable.js
    
    https://github.com/IlchCMS/Ilch-2.0/issues/602
    
    * Move venobox to static folder of gallery module
    
    Currently this is the only (and optional) module that uses it.

--- application/modules/gallery/controllers/admin/Index.php
+++ application/modules/gallery/controllers/admin/Index.php
@@ -79,8 +79,8 @@ class Index extends \Ilch\Controller\Admin
                     $sortArray = [];
 
                     foreach ($sortItems as $sortItem) {
-                        if ($sortItem->item_id !== null) {
-                            $sortArray[$sortItem->item_id] = (int)$sortItem->parent_id;
+                        if ($sortItem->id !== null) {
+                            $sortArray[$sortItem->id] = (int)$sortItem->parent_id;
                         }
                     }
 
