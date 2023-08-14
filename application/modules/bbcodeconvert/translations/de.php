efc01b0b4ca1f7cced6a8e1153dd40d8e17d846f
blackcoder87 <blackcoder@gmx.de>
2023-08-14 18:28:27 +0200

    bbcodeconvert: add translation for privatlayout

--- application/modules/bbcodeconvert/translations/de.php
+++ application/modules/bbcodeconvert/translations/de.php
@@ -14,6 +14,7 @@ return [
     'radiohoerercharts' => 'Radio Hörer Charts',
     'teams' => 'Teams',
     'user' => 'Benutzer',
+    'privatlayout' => 'Ilch-Privat',
     'menuConvert' => 'BBCode-Konvertierung',
     'menuOverview' => 'Übersicht',
     'menuNote' => 'Hinweis',
--- application/modules/bbcodeconvert/translations/en.php
+++ application/modules/bbcodeconvert/translations/en.php
@@ -14,6 +14,7 @@ return [
     'radiohoerercharts' => 'Radio Listener Charts',
     'teams' => 'Teams',
     'user' => 'User',
+    'privatlayout' => 'Ilch-Privat',
     'menuConvert' => 'BBCode conversion',
     'menuOverview' => 'Overview',
     'menuNote' => 'About',
