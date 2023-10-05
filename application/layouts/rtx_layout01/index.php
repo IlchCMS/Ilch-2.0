e3a59c62a67189ec495d33ac73618e022b470ef3
RTX2070 <barth.duvenstedt@gmx.de>
2023-10-05 20:37:19 +0200

    Navigation mit Dropdown
    Es wurden 2 Menüs hinzugefügt.
    
    Menü 1, zeigt Menüpunkte separat an.
    Menü 2, zeigt das Menü als Dropdown an.

--- application/layouts/rtx_layout01/index.php
+++ application/layouts/rtx_layout01/index.php
@@ -15,10 +15,29 @@
         <div class="container d-flex align-items-center">
 
             <!-- ToDo:: Später hat man die Wahl zwischen LogoGrafik oder einfachem Text -->
-            <h1 class="me-auto"><a href="<?=$this->getUrl() ?>" class="logo me-auto"><img src="<?= $this->getStaticUrl('img/ilch_logo.png') ?>" alt="Ilch2-Logo" class="img-fluid">Ilch CMS</a></h1>
-
+            <a href="<?=$this->getUrl() ?>" class="me-auto"><img src="<?= $this->getStaticUrl('img/ilch_logo.png') ?>" alt="Ilch2-Logo" class="img-fluid"></a>
+            <!-- ======= Navbar ======= -->
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <?=$this->getMenu(1, '%c', [
+                        'boxes' => [
+                            'render' => false
+                        ],
+                    ])
+                    ?>
+                    <?=$this->getMenu(2, '<li class="dropdown"><a class="nav-link" href="#" title="%s">%s</a>%c</li>', [
+                        'menus' => [
+                            'li-class-root'  => 'dropdown',
+                            'allow-nesting' => false
+                        ],
+                        'boxes' => [
+                            'render' => false
+                        ],
+                    ]) ?>
+                </ul>
+            </nav>
+            <!-- ======= Ende Navbar ======= -->
         </div>
-
     </header>
 <!-- ======= Ende Header ======= -->
 </body>
