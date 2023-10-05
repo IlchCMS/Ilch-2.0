45a36978f31388e1870b2846e06cbc762919d6c2
RTX2070 <barth.duvenstedt@gmx.de>
2023-10-05 18:08:02 +0200

    Fehlerbeseitigung und Anlegen der style.css
    Kleinen Fehler beseitigt.
    CSS-Datei angelegt mit generellen Einstellungen für das Layout ansich.

--- application/layouts/rtx_layout01/index.php
+++ application/layouts/rtx_layout01/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <?= $this->getHeader() ?>
     <link href="<?= $this->getVendorUrl('twbs/bootstrap/dist/css/bootstrap.min.css') ?>" rel="stylesheet">
-    <link href="<?= $this->getLayoutUrl('cssstyle.css') ?>" rel="stylesheet">
+    <link href="<?= $this->getLayoutUrl('css/style.css') ?>" rel="stylesheet">
     <?= $this->getCustomCSS() ?>
     <script src="<?= $this->getVendorUrl('twbs/bootstrap/dist/js/bootstrap.bundle.min.js') ?>"></script>
 </head>
