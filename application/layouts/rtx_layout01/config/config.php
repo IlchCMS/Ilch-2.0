f08fe01583e1a2c2d446b005032bb0b09bde01dd
RTX2070 <barth.duvenstedt@gmx.de>
2023-10-05 17:41:30 +0200

    NeuesLayoutBS5 - config
    Config.php, damit das Layout installiert werden kann.

---
+++ application/layouts/rtx_layout01/config/config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @copyright Ilch 2
+ * @package ilch
+ */
+
+namespace Layouts\rtx_layout01\Config;
+
+class Config extends \Ilch\Config\Install
+{
+    public $config = [
+        'name' => 'rtx_layout01',
+        'version' => '1.0.0',
+        'author' => 'RTX2070',
+        'link' => 'http://ilch.de',
+        'desc' => 'Bootstrap 5 Standard Layout (flexbox)',
+        
+        //'modulekey' => 'Name of Module'
+    ];
+
+    public function getUpdate($installedVersion)
+    {
+    }
+}
