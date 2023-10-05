e7934f4a99fd917a9ff1d46a4fef3eaa2849e9f9
RTX2070 <barth.duvenstedt@gmx.de>
2023-10-05 17:50:47 +0200

    Update index.php
    HTML Grundgerüst für Ilch-CMS und Testausgabe

--- application/layouts/rtx_layout01/index.php
+++ application/layouts/rtx_layout01/index.php
@@ -1,1 +1,15 @@
 <?php
+/** @var $this \Ilch\Layout\Frontend */ ?><!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <?= $this->getHeader() ?>
+    <link href="<?= $this->getVendorUrl('twbs/bootstrap/dist/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?= $this->getLayoutUrl('cssstyle.css') ?>" rel="stylesheet">
+    <?= $this->getCustomCSS() ?>
+    <script src="<?= $this->getVendorUrl('twbs/bootstrap/dist/js/bootstrap.bundle.min.js') ?>"></script>
+</head>
+<body>
+<h1>Testausgabe</h1>
+</body>
+</html>
