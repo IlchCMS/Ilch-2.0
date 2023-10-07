c0f8db34cf6f8e288765f06f3a13476271800578
Zirngast Rene <52356917+Slipi85@users.noreply.github.com>
2023-10-07 13:52:58 +0200

    Fixed CSS and Layout
    Fixed Forum.css
    Fixed stylre.css from clan3colum
    Update index.php and index_full.php from clan3columns | class .panal-gaming and .panel-heading to .card-gaming and card.header

--- application/layouts/clan3columns/index_full.php
+++ application/layouts/clan3columns/index_full.php
@@ -20,8 +20,8 @@
               <?php
               echo $this->getMenu(
                   1,
-                  '<div class="card panel-gaming">
-                       <div class="panel-heading">%s</div>
+                  '<div class="card card-gaming">
+                       <div class="card-header">%s</div>
                           <div class="card-body">
                               %c
                           </div>
@@ -75,8 +75,8 @@
                       <?php
                       echo $this->getMenu(
                           1,
-                          '<div class="card panel-gaming">
-                               <div class="panel-heading">%s</div>
+                          '<div class="card card-gaming">
+                               <div class="card-header">%s</div>
                                   <div class="card-body">
                                       %c
                                   </div>
@@ -86,7 +86,7 @@
                     </div>
                     <div class="col-xs-12 col-md-3 col-lg-10">
                       <?=$this->getHmenu() ?>
-                      <div class="card panel-default">
+                      <div class="card card-default">
                           <div class="card-body">
                               <?=$this->getContent() ?>
                           </div>
