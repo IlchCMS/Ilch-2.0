c9e99d7a3ea60d5a65abd4051c46701b57f61644
blackcoder87 <blackcoder@gmx.de>
2023-10-29 06:23:41 +0100

    Fix indentations

--- application/modules/media/views/admin/iframe/indexckeditor.php
+++ application/modules/media/views/admin/iframe/indexckeditor.php
@@ -3,58 +3,58 @@
 <?php if ($this->get('medias') != ''): ?>
 <div id="ilchmedia" class="container-fluid">
     <?php if ($this->getRequest()->getParam('type') === 'imageckeditor' || $this->getRequest()->getParam('type') === 'single'): ?>
-      <div class="row">
+        <div class="row">
         <?php foreach ($this->get('medias') as $media): ?>
             <?php if (in_array($media->getEnding(), explode(' ',$this->get('media_ext_img')))): ?>
-              <div class="row">
-                <div  id="<?=$media->getId() ?>" class="col-xl-2 col-md-3 col-4 media_loader">
-                    <img class="image img-thumbnail img-fluid"
-                         data-url="<?=$media->getUrl() ?>"
-                         src="<?=$this->getBaseUrl($media->getUrlThumb()) ?>"
-                         alt="<?=$media->getName() ?>">
+                <div class="row">
+                    <div  id="<?=$media->getId() ?>" class="col-xl-2 col-md-3 col-4 media_loader">
+                        <img class="image img-thumbnail img-fluid"
+                             data-url="<?=$media->getUrl() ?>"
+                             src="<?=$this->getBaseUrl($media->getUrlThumb()) ?>"
+                             alt="<?=$media->getName() ?>">
+                    </div>
                 </div>
-              </div>
             <?php endif; ?>
         <?php endforeach; ?>
         </div>
     <?php endif; ?>
 
     <?php if ($this->getRequest()->getParam('type') === 'media' || $this->getRequest()->getParam('type') === 'videockeditor'): ?>
-      <div class="row">
+        <div class="row">
         <?php foreach ($this->get('medias') as $media): ?>
             <?php if (in_array($media->getEnding(), explode(' ',$this->get('media_ext_video')))): ?>
-              <div class="row">
-                <div id="<?=$media->getId() ?>" class="col-xl-2 col-lg-2 col-md-3 col-4 co thumb media_loader">
-                    <img class="image img-thumbnail img-fluid"
-                         data-url="<?=$media->getUrl() ?>"
-                         src="<?=$this->getBaseUrl('application/modules/media/static/img/nomedia.png') ?>"
-                         alt="<?=$media->getName() ?>">
-                    <div class="text-right">
-                        <small class="text-info"><?=substr($media->getName(), 0, 20) ?></small>
+                <div class="row">
+                    <div id="<?=$media->getId() ?>" class="col-xl-2 col-lg-2 col-md-3 col-4 co thumb media_loader">
+                        <img class="image img-thumbnail img-fluid"
+                             data-url="<?=$media->getUrl() ?>"
+                             src="<?=$this->getBaseUrl('application/modules/media/static/img/nomedia.png') ?>"
+                             alt="<?=$media->getName() ?>">
+                        <div class="text-right">
+                            <small class="text-info"><?=substr($media->getName(), 0, 20) ?></small>
+                        </div>
                     </div>
                 </div>
-              </div>
             <?php endif; ?>
         <?php endforeach; ?>
         </div>
     <?php endif; ?>
 
     <?php if ($this->getRequest()->getParam('type') === 'file' || $this->getRequest()->getParam('type') === 'fileckeditor'): ?>
-      <div class="row">
+        <div class="row">
         <?php foreach ($this->get('medias') as $media): ?>
             <?php if (in_array($media->getEnding(), explode(' ',$this->get('media_ext_file')))): ?>
-              <div class="row">
-                <div id="<?=$media->getId() ?>" class="col-xl-2 col-lg-2 col-md-3 col-4 co thumb media_loader">
-                    <img class="image img-thumbnail img-fluid"
-                         data-alt="<?=$media->getName() ?>"
-                         data-url="<?=$media->getUrl() ?>"
-                         src="<?=$this->getBaseUrl('application/modules/media/static/img/nomedia.png') ?>"
-                         alt="<?=$media->getName() ?>">
-                    <div class="text-right">
-                        <small class="text-info"><?=substr($media->getName(), 0, 20) ?></small>
+                <div class="row">
+                    <div id="<?=$media->getId() ?>" class="col-xl-2 col-lg-2 col-md-3 col-4 co thumb media_loader">
+                        <img class="image img-thumbnail img-fluid"
+                             data-alt="<?=$media->getName() ?>"
+                             data-url="<?=$media->getUrl() ?>"
+                             src="<?=$this->getBaseUrl('application/modules/media/static/img/nomedia.png') ?>"
+                             alt="<?=$media->getName() ?>">
+                        <div class="text-right">
+                            <small class="text-info"><?=substr($media->getName(), 0, 20) ?></small>
+                        </div>
                     </div>
                 </div>
-              </div>
             <?php endif; ?>
         <?php endforeach; ?>
         </div>
