adde2cffe4b9050cb8b7d81a90da0a98ef00fe5f
Zirngast Rene <52356917+Slipi85@users.noreply.github.com>
2023-10-30 14:51:18 +0100

    Fixed (#844)
    Fixed Layout
    Fixed admin.css

--- application/layouts/clan3columns/index.php
+++ application/layouts/clan3columns/index.php
@@ -62,7 +62,7 @@
                   </div>
 
                   <div class="gaming-name">
-                      <?=$this->getLayoutSetting('header')?>
+                      <?=$this->getLayoutSetting('headertext')?>
                   </div>
               </div>
           </div>
@@ -71,7 +71,7 @@
         <div class="container">
             <div class="gaming">
                 <div class="row">
-                    <div class="d-none d-md-block col-md-3 col-lg-2">
+                    <div class="d-none d-md-block col-lg-3 col-xl-2">
                         <?php
                         echo $this->getMenu(
                             1,
@@ -84,7 +84,7 @@
                         );
                         ?>
                     </div>
-                    <div class="col-xs-12 col-md-7 col-lg-8">
+                    <div class="col-12 col-lg-7 col-xl-8">
                         <?=$this->getHmenu() ?>
                         <div class="card card-default">
                             <div class="card-body">
@@ -92,7 +92,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-xs-12 col-md-2">
+                    <div class="col-12 col-lg-2">
                         <?php
                         echo $this->getMenu(
                             2,
@@ -113,10 +113,10 @@
             <div class="container">
                 <div class="gaming">
                     <div class="row">
-                        <div class="col-xs-12 col-md-6">
+                        <div class="col-12 col-lg-6">
                             &copy; Ilch 2.0 Free Layout | CMS by <a href="https://www.ilch.de/">Ilch</a>
                         </div>
-                        <div class="col-xs-12 col-md-6 nav">
+                        <div class="col-12 col-lg-6 nav">
                             <ul>
                                 <li><a href="<?=$this->getUrl() ?>"><?=$this->getTrans('home') ?></a></li>
                                 <li><a href="<?=$this->getUrl(['module'=>'contact', 'controller'=>'index', 'action'=>'index']) ?>"><?=$this->getTrans('contact') ?></a></li>
