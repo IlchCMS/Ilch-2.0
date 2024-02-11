da46e81e217af4e144126b8e7e4794c74a72bff7
blackcoder87 <blackcoder@gmx.de>
2024-02-11 18:20:15 +0100

    Install: Fix database name validation (#924)
    Added a check for the prefix and some info messages regarding the prefix and db name.

--- application/modules/install/views/index/database.php
+++ application/modules/install/views/index/database.php
@@ -9,32 +9,36 @@ $database = $this->get('database')
     <label for="dbName" class="col-lg-3 control-label">
         <?=$this->getTrans('dbName') ?>:
     </label>
-    <div class="col-lg-9">
-        <?php if (count($database) > 0) : ?>
+    <?php if (count($database) > 0) : ?>
+        <div class="col-lg-9">
             <select class="form-control" id="dbName" name="dbName">
                 <option value=""><?=$this->getTrans('selectDatabase') ?></option>
                 <?php foreach ($database as $value) : ?>
                     <option <?=$this->originalInput('dbName', $this->get('dbName')) == $value ? 'selected="selected"' : '' ?> value="<?=$value ?>"><?=$this->escape($value) ?></option>
                 <?php endforeach; ?>
             </select>
-        <?php else : ?>
+        </div>
+    <?php else : ?>
+        <div class="col-lg-9 input-group">
             <input type="text"
                    class="form-control"
                    id="dbName"
                    name="dbName"
                    value="<?=$this->escape($this->originalInput('dbName', $this->get('dbName'))) ?>" />
-        <?php endif; ?>
-    </div>
+            <div class="input-group-addon" rel="tooltip" title="<?=$this->getTrans('dbNameInfo') ?>"><i class="fa-solid fa-circle-info"></i></div>
+        </div>
+    <?php endif; ?>
 </div>
 <div class="form-group <?=$this->validation()->hasError('dbPrefix') ? 'has-error' : '' ?>">
     <label for="dbPrefix" class="col-lg-3 control-label">
         <?=$this->getTrans('dbPrefix') ?>:
     </label>
-    <div class="col-lg-9">
+    <div class="col-lg-9 input-group">
         <input type="text"
                class="form-control"
                id="dbPrefix"
                name="dbPrefix"
                value="<?=$this->escape($this->originalInput('dbPrefix', $this->get('dbPrefix'))) ?>" />
+        <div class="input-group-addon" rel="tooltip" title="<?=$this->getTrans('dbPrefixInfo') ?>"><i class="fa-solid fa-circle-info"></i></div>
     </div>
 </div>
