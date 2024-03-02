e36959bf7f3b888d311c798f14b3f99807a9709a
blackcoder87 <blackcoder@gmx.de>
2024-03-02 12:00:42 +0100

    Update indexckeditor.php

--- application/modules/media/views/admin/iframe/indexckeditor.php
+++ application/modules/media/views/admin/iframe/indexckeditor.php
@@ -61,8 +61,7 @@
 <?php if ($this->getRequest()->getParam('type') === 'imageckeditor' || $this->getRequest()->getParam('type') === 'single'): ?>
     <script>
     $(".image").click(function() {
-        var dialog = window.top.CKEDITOR.dialog.getCurrent();
-        dialog.setValueOf('tab-basic','src', '<?=$this->getBaseUrl() ?>'+$(this).data('url'));
+        window.top.$('#mediaModal').attr('url', '<?=$this->getBaseUrl() ?>'+$(this).data('url'));
         window.top.$('#mediaModal').modal('hide');
     });
     </script>
@@ -70,9 +69,7 @@
 <?php if ($this->getRequest()->getParam('type') === 'file' || $this->getRequest()->getParam('type') === 'fileckeditor'): ?>
     <script>
     $(".image").click(function() {
-        var dialog = window.top.CKEDITOR.dialog.getCurrent();
-        dialog.setValueOf('tab-adv','file', '<?=$this->getBaseUrl() ?>'+$(this).data('url'));
-        dialog.setValueOf('tab-adv','alt', $(this).data('alt'));
+        window.top.$('#mediaModal').attr('url', '<?=$this->getBaseUrl() ?>'+$(this).data('url'));
         window.top.$('#mediaModal').modal('hide');
     });
     </script>
@@ -80,8 +77,7 @@
 <?php if ($this->getRequest()->getParam('type') === 'media' || $this->getRequest()->getParam('type') === 'videockeditor'): ?>
     <script>
     $(".image").click(function() {
-        var dialog = window.top.CKEDITOR.dialog.getCurrent();
-        dialog.setValueOf('tab-mov','video', '<?=$this->getBaseUrl() ?>'+$(this).data('url'));
+        window.top.$('#mediaModal').attr('url', '<?=$this->getBaseUrl() ?>'+$(this).data('url'));
         window.top.$('#mediaModal').modal('hide');
     });
     </script>
