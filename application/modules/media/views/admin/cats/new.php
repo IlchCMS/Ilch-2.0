882d923560404a63e74d2c44c9d60e21b5dddb18
blackcoder87 <blackcoder@gmx.de>
2024-04-05 06:58:05 +0200

    Remove no longer existing Bootstrap class "form-group-options"
    Use a custom one as identifier for the function.

--- application/modules/media/views/admin/cats/new.php
+++ application/modules/media/views/admin/cats/new.php
@@ -15,7 +15,7 @@ div.input-group {
 <h1><?=$this->getTrans('newCat') ?></h1>
 <form method="POST">
     <?=$this->getTokenField() ?>
-    <div class="row mb-3 form-group-options">
+    <div class="row mb-3 category-input-group">
         <div class="col-xl-6">
             <div class="input-group">
                 <input type="text" class="form-control" name="title_option[]" placeholder="<?=$this->getTrans('catTitle') ?>">
@@ -30,13 +30,13 @@ div.input-group {
 
 <script>
 $(function() {
-    $(document).on('focus', 'div.form-group-options div.input-group:last-child input', function() {
+    $(document).on('focus', 'div.category-input-group div.input-group:last-child input', function() {
         var sInputGroupHtml = $(this).parent().html();
         var sInputGroupClasses = $(this).parent().attr('class');
         $(this).parent().parent().append('<div class="'+sInputGroupClasses+'">'+sInputGroupHtml+'</div>');
     });
 
-    $(document).on('click', 'div.form-group-options .input-group-text-remove', function() {
+    $(document).on('click', 'div.category-input-group .input-group-text-remove', function() {
         $(this).parent().remove();
     });
 });
