627ec6ae03305e544c79e7d8e4be71a9ed15329a
blackcoder87 <blackcoder@gmx.de>
2024-04-15 15:51:45 +0200

    Checkoutbasic: Add form-select class

--- application/modules/checkoutbasic/views/admin/index/settings.php
+++ application/modules/checkoutbasic/views/admin/index/settings.php
@@ -5,18 +5,18 @@
 <h1><?=$this->getTrans('accountdata') ?></h1>
 <form method="POST" action="">
     <?=$this->getTokenField() ?>
-    <div class="row mb-3 <?=$this->validation()->hasError('checkoutContact') ? 'has-error' : '' ?>">
+    <div class="row mb-3<?=$this->validation()->hasError('checkoutContact') ? ' has-error' : '' ?>">
         <textarea class="form-control ckeditor"
                   id="ck_1"
                   toolbar="ilch_html"
                   name="checkoutContact"><?=$this->get('checkoutContact') ?></textarea>
     </div>
-    <div class="row mb-3 <?=$this->validation()->hasError('checkoutCurrency') ? 'has-error' : '' ?>">
+    <div class="row mb-3<?=$this->validation()->hasError('checkoutCurrency') ? ' has-error' : '' ?>">
         <label for="checkoutCurrency" class="col-form-label col-xl-2">
             <?=$this->getTrans('checkoutCurrency') ?>:
         </label>
         <div class="col-2">
-            <select name="checkoutCurrency" id="checkoutCurrency">
+            <select name="checkoutCurrency" id="checkoutCurrency" class="form-select">
                 <?php
                 foreach ($this->get('currencies') ?? [] as $currency) {
                     if ($this->get('checkoutCurrency') != $currency->getId()) {
