4a3483e593f4885caa3cac6a7676f585471f3b85
blackcoder87 <blackcoder@gmx.de>
2024-06-19 17:43:17 +0200

    bbcodeconvert: update for ilch 2.2.1 (#983)

--- application/modules/bbcodeconvert/config/config.php
+++ application/modules/bbcodeconvert/config/config.php
@@ -13,7 +13,7 @@ class Config extends Install
 {
     public $config = [
         'key' => 'bbcodeconvert',
-        'version' => '1.1.0',
+        'version' => '1.1.1',
         'icon_small' => 'fa-solid fa-arrow-right-arrow-left',
         'author' => 'ilch.de',
         'link' => 'https://www.ilch.de',
