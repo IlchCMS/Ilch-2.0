ab7b32418d55ffef5be7be4fe35396a4201f5940
blackcoder87 <blackcoder@gmx.de>
2024-06-22 19:28:07 +0200

    CKEditor 5: Cleanup directory

--- static/js/ckeditor5/build/plugins/ilchmedia/ilchmedia.d.ts
+++
@@ -1,5 +0,0 @@
-export default class IlchMedia extends Plugin {
-    static get requires(): (typeof IlchMediaUI)[];
-}
-import { Plugin } from '@ckeditor/ckeditor5-core';
-import IlchMediaUI from './ilchmediaui';
