d16eb65c520b6f6e9eca85e0fc2532db1de23f3f
blackcoder87 <blackcoder@gmx.de>
2024-06-22 19:37:19 +0200

    CKEditor 5: Cleanup directory (#992)

--- static/js/ckeditor5/build/plugins/ckeditor5-emoji/src/ui/characterinfoview.d.ts
+++
@@ -1,4 +0,0 @@
-export default class CharacterInfoView extends View<HTMLElement> {
-    constructor(locale: any);
-}
-import { View } from '@ckeditor/ckeditor5-ui';
--- static/js/ckeditor5/build/plugins/ilchps/plugin.d.ts
+++
@@ -1,1 +0,0 @@
-export function IlchPs(editor: any): void;
