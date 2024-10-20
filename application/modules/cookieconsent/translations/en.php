d30467b8249ec7e24f176bd9371e1c02aeecc7cf
Dennis Reilard <d.reilard@web.de>
2024-10-20 16:13:52 +0200

    Cookieconsent-Modul: Nutze Tarteaucitron anstatt Cookieconsent (#1070)
    - Tarteaucitron anstatt Cookieconsent

--- application/modules/cookieconsent/translations/en.php
+++ application/modules/cookieconsent/translations/en.php
@@ -1,17 +1,15 @@
 <?php
+
 /**
- * @copyright Ilch 2.0
+ * @copyright Ilch 2
  * @package ilch
  */
 
 return [
     'menuCookieConsent' => 'Cookie Consent',
     'cookieConsentShow' => 'Show',
-    'cookieConsentLayout' => 'Layout',
-    'cookieConsentLayoutBlock' => 'Block',
-    'cookieConsentLayoutClassic' => 'Classic',
-    'cookieConsentLayoutEdgeless' => 'Edgeless',
     'cookieConsentPos' => 'Position',
+    'cookieIconPos' => 'Icon Position',
     'cookieConsentPopUp' => 'PopUp Style',
     'cookieConsentPopUpBGColor' => 'Background color',
     'cookieConsentPopUpTextColor' => 'Text color',
@@ -23,9 +21,13 @@ return [
     'cookieConsentBtnBGColor' => 'Background color',
     'cookieConsentBtnTextColor' => 'Text color',
     'cookieConsentPosTop' => 'Top',
-    'cookieConsentPosFloatingLeft' => 'Bottom left',
-    'cookieConsentPosFloatingRight' => 'Bottom right',
+    'cookieConsentPosMiddle' => 'Middle',
     'cookieConsentPosBottom' => 'Bottom',
+    'cookieConsentPosPopup' => 'Popup',
+    'cookieIconBottomRight' => 'Bottom Right',
+    'cookieIconBottomLeft' => 'Bottom Left',
+    'cookieIconTopRight' => 'Top Right',
+    'cookieIconTopLeft' => 'Top Left',
     'cookieConsentLocale' => 'Locale',
     'cookieConsentDesc' => 'Description',
     'cookieConsentText' => 'Consent',
