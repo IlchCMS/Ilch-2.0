9c7bfea8f2d5db3d0a64cf1807c650e899531862
blackcoder87 <blackcoder@gmx.de>
2024-10-30 16:18:02 +0100

    Use queryMulti instead of query

--- application/modules/user/config/config.php
+++ application/modules/user/config/config.php
@@ -969,7 +969,7 @@ class Config extends \Ilch\Config\Install
                 // Add new opt_comments and admin_comments column to the users table.
                 // opt_comments: Whether the user allows comments on the profile or not.
                 // admin_comments: Whether the admin allows comments on the profile of this user or not.
-                $this->db()->query('ALTER TABLE `[prefix]_users` ADD COLUMN `opt_comments` TINYINT(1) DEFAULT 1 AFTER `opt_mail`;
+                $this->db()->queryMulti('ALTER TABLE `[prefix]_users` ADD COLUMN `opt_comments` TINYINT(1) DEFAULT 1 AFTER `opt_mail`;
                         ALTER TABLE `[prefix]_users` ADD COLUMN `admin_comments` TINYINT(1) DEFAULT 1 AFTER `opt_comments`;');
 
                 // Add new setting for comments on profiles (globally) and allow them by default.
