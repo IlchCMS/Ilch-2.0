b6113564fd4c5d644bbaa622819ff89cf4bf9330
blackcoder87 <blackcoder@gmx.de>
2024-11-03 09:57:45 +0100

    Issue 509: Follow-up to fix  "empty needle" (#1086)

--- application/modules/user/views/profil/index.php
+++ application/modules/user/views/profil/index.php
@@ -94,10 +94,12 @@ foreach ($profil->getGroups() as $group) {
                 // Using this code instead of for example str_replace to only replace the addition once.
                 // Example: addition = "https://discord.gg/", value = "https://discord.gg/username". The user entered the complete URL instead of just the username.
                 $profileFieldContentValue = $profileFieldContent->getValue();
-                $pos = strpos($profileFieldContent->getValue(), $profileIconField->getAddition());
+                if ($profileIconField->getAddition()) {
+                    $pos = strpos($profileFieldContent->getValue(), $profileIconField->getAddition());
 
-                if ($pos !== false) {
-                    $profileFieldContentValue = substr_replace($profileFieldContent->getValue(), '', $pos, $profileIconField->getAddition());
+                    if ($pos !== false) {
+                        $profileFieldContentValue = substr_replace($profileFieldContent->getValue(), '', $pos, $profileIconField->getAddition());
+                    }
                 }
 
                 echo '<a href="' . $profileFieldContentValue . '" target="_blank" rel="noopener" class="' . $profileIconField->getIcon() . ' fa-lg user-link" title="' . $profileFieldName . '"></a>';
