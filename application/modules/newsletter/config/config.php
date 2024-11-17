06fa6243ff3ab5514df4263cec883c0327ddd1ad
blackcoder87 <blackcoder@gmx.de>
2024-11-17 17:22:38 +0100

    Newsletter: Redirect guest to login page
    Set needed values for the Double-Opt-In feature.

--- application/modules/newsletter/config/config.php
+++ application/modules/newsletter/config/config.php
@@ -13,7 +13,7 @@ class Config extends Install
 {
     public $config = [
         'key' => 'newsletter',
-        'version' => '1.8.0',
+        'version' => '1.8.1',
         'icon_small' => 'fa-regular fa-newspaper',
         'author' => 'Veldscholten, Kevin',
         'link' => 'https://ilch.de',
