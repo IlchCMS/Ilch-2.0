afab09af400481de7e0284336782a5421738eaa4
blackcoder87 <blackcoder@gmx.de>
2025-04-05 08:50:30 +0200

    Update comment [skip ci]

--- application/modules/admin/config/config.php
+++ application/modules/admin/config/config.php
@@ -1230,7 +1230,7 @@ class Config extends \Ilch\Config\Install
                 replaceVendorDirectory();
                 break;
             case "2.2.10":
-                // Update vendor folder to update Bootstrap to version 5.3.4.
+                // Update vendor folder to update Bootstrap to version 5.3.5.
                 replaceVendorDirectory();
                 break;
         }
