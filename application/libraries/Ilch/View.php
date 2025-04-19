b56467f67360a1e24fe23506708003f5aefac407
blackcoder87 <blackcoder@gmx.de>
2025-04-19 08:49:12 +0200

    Fix "Argument must be of the type string"

--- application/libraries/Ilch/View.php
+++ application/libraries/Ilch/View.php
@@ -41,7 +41,7 @@ class View extends Design\Base
         );
         $view->setArray($data);
 
-        echo $view->loadScript(APPLICATION_PATH.'/modules/'.$request->getModuleName().'/views/'.$file);
+        echo $view->loadScript(APPLICATION_PATH . '/modules/' . $request->getModuleName() . '/views/' . $file);
     }
 
     /**
@@ -55,13 +55,13 @@ class View extends Design\Base
     public function getSaveBar(string $saveKey = 'saveButton', string $nameKey = null, string $deleteKey = ''): string
     {
         $html = '<div class="content_savebox">
-                    <button type="submit" class="save_button btn btn-secondary" name="save'.$nameKey.'" value="save">
-                        '.$this->getTrans($saveKey).'
+                    <button type="submit" class="save_button btn btn-secondary" name="save' . $nameKey . '" value="save">
+                        ' . $this->getTrans($saveKey) . '
                     </button>';
 
         if (!empty($deleteKey)) {
             $html .= '<button type="submit" class="delete_button btn float-end" name="delete" value="delete">
-                        '.$this->getTrans($deleteKey).'
+                        ' . $this->getTrans($deleteKey) . '
                       </button>';
         }
 
@@ -84,7 +84,7 @@ class View extends Design\Base
                             <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-bs-toggle="dropdown">' . $this->getTrans('selected') . '</button>
                             <ul class="dropdown-menu listChooser" role="menu">';
         foreach ($actions as $key => $name) {
-            $html .= '<li><a href="#" class="dropdown-item" data-hiddenkey="'.$key.'" id="'.$key.'">'.$this->getTrans($name).'</a></li>';
+            $html .= '<li><a href="#" class="dropdown-item" data-hiddenkey="' . $key . '" id="' . $key . '">' . $this->getTrans($name) . '</a></li>';
         }
         $html .= '</ul></div></div>';
 
@@ -99,7 +99,7 @@ class View extends Design\Base
      */
     public function getEditIcon(array $url): string
     {
-        return '<a href="'.$this->getUrl($url).'" title="'.$this->getTrans('edit').'"><span class="fa-solid fa-pen-to-square text-success"></span></a>';
+        return '<a href="' . $this->getUrl($url) . '" title="' . $this->getTrans('edit') . '"><span class="fa-solid fa-pen-to-square text-success"></span></a>';
     }
 
     /**
@@ -110,7 +110,7 @@ class View extends Design\Base
      */
     public function getDeleteIcon(array $url): string
     {
-        return '<a href="'.$this->getUrl($url, null, true).'" title="'.$this->getTrans('delete').'" class="delete_button"><span class="fa-regular fa-trash-can text-danger"></span></a>';
+        return '<a href="' . $this->getUrl($url, null, true) . '" title="' . $this->getTrans('delete') . '" class="delete_button"><span class="fa-regular fa-trash-can text-danger"></span></a>';
     }
 
     /**
@@ -122,7 +122,7 @@ class View extends Design\Base
      */
     public function getDeleteCheckbox(string $name, string $id): string
     {
-        return '<input type="checkbox" name="'.$name.'[]" value="'.$id.'" />';
+        return '<input type="checkbox" name="' . $name . '[]" value="' . $id . '" />';
     }
 
     /**
@@ -133,18 +133,18 @@ class View extends Design\Base
      */
     public function getCheckAllCheckbox(string $childs): string
     {
-        return '<input type="checkbox" class="check_all" data-childs="'.$childs.'" />';
+        return '<input type="checkbox" class="check_all" data-childs="' . $childs . '" />';
     }
 
     /**
      * Returns the input data from the last request.
      *
      * @param string|null $key     Array key
-     * @param string $default Default value if key not found
+     * @param mixed $default Default value if key not found
      *
      * @return mixed
      */
-    public function originalInput(string $key = null, string $default = '')
+    public function originalInput(?string $key = null, $default = '')
     {
         return $this->getRequest()->getOldInput($key, $default);
     }
