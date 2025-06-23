131a10058949383629571648b7230ad5ce259e10
Dennis Reilard <d.reilard@web.de>
2025-06-23 07:12:58 +0200

    Update index.php

--- application/modules/away/views/admin/settings/index.php
+++ application/modules/away/views/admin/settings/index.php
@@ -39,7 +39,9 @@
         </label>
         <div class="col-xl-4">
             <select class="choices-select form-control" id="notifyGroups" name="notifyGroups[]" data-placeholder="<?=$this->getTrans('selectAssignedGroups') ?>" multiple>
-                <?php foreach ($this->get('userGroupList') as $groupList) : ?>
+                <?php
+                /** @var \Modules\User\Models\Group $groupList */
+                foreach ($this->get('userGroupList') as $groupList) : ?>
                     <option value="<?=$groupList->getId() ?>"<?=(in_array($groupList->getId(), $this->get('groups'))) ? ' selected' : '' ?>><?=$groupList->getName() ?></option>
                 <?php endforeach; ?>
             </select>
