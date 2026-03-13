5397e594f151e6a78e2d86d71e84b6da337cdf19
blackcoder87 <blackcoder@gmx.de>
2026-03-13 15:00:12 +0100

    Add captive-it updateserver (#1324)

--- application/modules/admin/config/config.php
+++ application/modules/admin/config/config.php
@@ -229,7 +229,8 @@ class Config extends \Ilch\Config\Install
                 PRIMARY KEY (`id`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1;
 
-            INSERT INTO `[prefix]_admin_updateservers` (`id`, `url`, `operator`, `country`) VALUES (1, "https://www.ilch.de/ilch2_updates/stable/", "ilch", "Germany");';
+            INSERT INTO `[prefix]_admin_updateservers` (`id`, `url`, `operator`, `country`) VALUES (1, "https://www.ilch.de/ilch2_updates/stable/", "ilch", "Germany");
+            INSERT INTO `[prefix]_admin_updateservers` (`id`, `url`, `operator`, `country`) VALUES (2, "https://update.captive-it.de/stable/", "BlackRomeo (ilch-Team)", "Germany");';
     }
 
     public function getUpdate(string $installedVersion): string
@@ -1278,6 +1279,19 @@ class Config extends \Ilch\Config\Install
                     $this->db()->insert('admin_updateservers')
                         ->values(['url' => 'https://www.ilch.de/ilch2_updates/stable/', 'operator' => 'ilch', 'country' => 'Germany'])
                         ->execute();
+                }
+
+                // Add new updateserver.
+                $this->db()->insert('admin_updateservers')
+                    ->values(['url' => 'https://update.captive-it.de/stable/', 'operator' => 'BlackRomeo (ilch-Team)', 'country' => 'Germany'])
+                    ->execute();
+
+                $databaseConfig = new \Ilch\Config\Database($this->db());
+                $currentUpdateserver = $databaseConfig->get('updateserver');
+
+                if ($currentUpdateserver === 'https://www.ilch.de/ilch2_updates/stable/') {
+                    $updateservers = ['https://www.ilch.de/ilch2_updates/stable/', 'https://update.captive-it.de/stable/'];
+                    $databaseConfig->set('updateserver', $updateservers[random_int(0, count($updateservers) - 1)]);
                 }
 
                 // Update vendor folder to update Bootstrap, Font Awesome, HTMLPurifier, jQuery, jQuery UI and PHPMailer.
