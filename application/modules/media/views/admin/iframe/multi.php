6cdd32c98ef04cfb2d77a0e62540f8db83e4fc38
PrivatePHPCoder <barth.duvenstedt@gmx.de>
2026-03-25 11:50:49 +0100

    Media: XSS in iframe/multi.php beheben – id-Parameter escaped, type-Parameter per json_encode in JS abgesichert

--- application/modules/media/views/admin/iframe/multi.php
+++ application/modules/media/views/admin/iframe/multi.php
@@ -18,11 +18,11 @@
 }
 </style>
 
-<form method="POST" action="<?=$_SESSION['media-url-action-button'] ?><?=$this->getRequest()->getParam('id') ?>">
+<form method="POST" action="<?=$_SESSION['media-url-action-button'] ?><?=$this->escape($this->getRequest()->getParam('id')) ?>">
     <?=$this->getTokenField() ?>
     <ul class="nav nav-pills navbar-fixed-top">
         <li class="nav-item"><a href="<?=$this->getUrl(['controller' => 'iframe', 'action' => 'upload', 'id' => $this->getRequest()->getParam('id')]) ?>" class="nav-link"><?=$this->getTrans('upload') ?></a></li>
-        <li class="nav-item"><a href="<?=$_SESSION['media-url-media-button'] ?><?=$this->getRequest()->getParam('id') ?>" class="nav-link"><?=$this->getTrans('media') ?></a></li>
+        <li class="nav-item"><a href="<?=$_SESSION['media-url-media-button'] ?><?=$this->escape($this->getRequest()->getParam('id')) ?>" class="nav-link"><?=$this->getTrans('media') ?></a></li>
         <li class="ms-auto nav-item"><button type="submit" class="btn btn-primary" name="save" value="save"><?=$this->getTrans('add') ?></button></li>
     </ul>
 
@@ -162,7 +162,7 @@
 $(document).ready(function() {
     function media_loader() {
         var ID=$(".media_loader:last").attr("id");
-        $.post("<?=$this->getUrl('admin/media/ajax/multi/type/') ?><?=$this->getRequest()->getParam('type') ?>/lastid/"+ID,
+        $.post("<?=$this->getUrl('admin/media/ajax/multi/type/') ?>"+<?=json_encode($this->getRequest()->getParam('type')) ?>+"/lastid/"+ID,
             function(data)
             {
                 if (data !== "") {
