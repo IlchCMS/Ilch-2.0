ad4c8c6e08d37a35e36264223ee7bc028eebd28a
blackcoder87 <blackcoder@gmx.de>
2026-03-29 14:43:34 +0200

    Media: pick part of a change from #1351

--- application/modules/media/views/admin/iframe/index.php
+++ application/modules/media/views/admin/iframe/index.php
@@ -85,13 +85,9 @@
 <?php endif; ?>
 
 <?php if ($this->getRequest()->getParam('type') === 'single'): ?>
-    <?php
-    $inputParam = $this->getRequest()->getParam('input');
-    $inputSuffix = ($inputParam !== null && $inputParam !== '') ? (int)$inputParam : '';
-    ?>
     <script>
     $(".image").click(function() {
-        window.top.$('#selectedImage<?=$inputSuffix ?>').val($(this).data('url'));
+        window.top.$('#selectedImage<?=$this->escape($this->getRequest()->getParam('input')) ?>').val($(this).data('url'));
         window.top.$('#mediaModal').modal('hide');
     });
     </script>
