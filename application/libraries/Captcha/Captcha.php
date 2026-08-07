17570775ace2a6c5f78753ed9bc1848845f1a2f5
RTX2070 <barth.duvenstedt@gmx.de>
2026-08-07 09:55:20 +0200

    Captcha: Ressourcen-Pfad absolut aufloesen (#1459)
    $resourcesPath ist mit dem relativen Wert 'resources' belegt. Das
    funktioniert bisher nur deshalb, weil Captcha.php ausschliesslich direkt
    per URL aufgerufen wird (siehe DefaultCaptcha::getCaptcha) und das
    Arbeitsverzeichnis dadurch im Ordner der Datei selbst liegt. Sobald die
    Klasse aus einem anderen Kontext geladen wird, zeigen Wortliste und
    Fontpfad ins Leere.
    
    Der Pfad wird jetzt im Property-Default ueber __DIR__ aufgeloest und
    haengt damit nicht mehr vom aktuellen Arbeitsverzeichnis ab. Ein
    Ueberschreiben der Property von aussen bleibt unveraendert moeglich.
    
    Co-authored-by: PrivatePHPCoder <189002751+PrivatePHPCoder@users.noreply.github.com>

--- application/libraries/Captcha/Captcha.php
+++ application/libraries/Captcha/Captcha.php
@@ -45,11 +45,13 @@ class Captcha
     /**
      * Path for resource files (fonts, words, etc.)
      *
-     * "resources" by default. For security reasons, is better move this
-     * directory to another location outise the web server
+     * The "resources" folder next to this file by default. Resolved absolutely
+     * so it does not depend on the current working directory. For security
+     * reasons, is better move this directory to another location outise the
+     * web server
      *
      */
-    public $resourcesPath = 'resources';
+    public $resourcesPath = __DIR__ . '/resources';
 
     /** Min word length (for non-dictionary random text generation) */
     public $minWordLength = 5;
