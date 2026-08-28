db051cb11f5e7b5ebbf224f0c53136364522c7e2
blackcoder87 <blackcoder@gmx.de>
2026-08-28 18:27:14 +0200

    Update index.php

--- application/modules/downloads/views/admin/index/index.php
+++ application/modules/downloads/views/admin/index/index.php
@@ -120,7 +120,7 @@ function rec(\Modules\Downloads\Models\DownloadsItem $item, \Ilch\View $obj): vo
                 <label for="access" class="col-xl-3 col-form-label">
                     <?=$this->getTrans('access') ?><br>
                     <span id="accessHelp" class="form-text">
-                      <?=$this->getTrans('accessEmptyHelp') ?>
+                        <?=$this->getTrans('accessEmptyHelp') ?>
                     </span>
                 </label>
                 <div class="col-xl-6">
