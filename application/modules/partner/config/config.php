9586d00bceca77bd59d632363c15f62579d63371
blackcoder87 <blackcoder@gmx.de>
2026-09-13 16:14:17 +0200

    Partner: Add unit tests and fix issues (#1509)
    Add unit tests
    Use correct font-awesome icon
    Other fixes

--- application/modules/partner/config/config.php
+++ application/modules/partner/config/config.php
@@ -11,7 +11,7 @@ class Config extends \Ilch\Config\Install
 {
     public $config = [
         'key' => 'partner',
-        'version' => '1.14.2',
+        'version' => '1.14.3',
         'icon_small' => 'fa-regular fa-handshake',
         'author' => 'Veldscholten, Kevin',
         'link' => 'https://ilch.de',
